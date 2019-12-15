d967e4806f0c9c540b2a6e088414011c4ccbe0bc
pyliu <3634168+pyliu@users.noreply.github.com>
2019-12-15 22:24:06 +0800

    wip

---
+++ components/nav-header.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="nav-top">
+        <nav>
+            <ul class="nav-list">
+                <nuxt-link to="/" tag="li" class="nav-link">Home</nuxt-link>
+                <nuxt-link to="/about" tag="li" class="nav-link">About</nuxt-link>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+.nav-top {
+  height: 4.5rem;
+  background-color: #00022d;
+}
+</style>
---
+++ pages/about/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <section class="about-page">
+        <h1>這是關於我頁面</h1>
+    </section>
+</template>
