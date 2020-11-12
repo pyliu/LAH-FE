242a81b4edcf1e09ba968ede3f827922f38b17d2
pyliu <3634168+pyliu@users.noreply.github.com>
2020-11-12 23:34:06 +0800

    use nuxt.js import axios => $axios

--- plugins/global-inject.js
+++ plugins/global-inject.js
@@ -1,28 +1,22 @@
-import axios from 'axios'
 import qs from 'qs'
 
-export default (nuxt, inject) => {
-  axios.defaults.transformRequest = [data => {
-    return qs.stringify(data)
-  }]
-  // leverage axios proxy in nuxt.config.js
-  axios.defaults.baseURL = '/api'
-
-  // axios.onResponse(response => {
-  //   this.cachedResponse = response.data
-  // })
-
-  // axios.onError(error => {
-  //   if (error.response.status === 500) {
-  //     nuxt.redirect('/sorry')
-  //   }
-  // })
-
-  // console.log(nuxt)
+export default ({ $axios }, inject) => {
+  $axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
+  // Add a request interceptor
+  $axios.interceptors.request.use(function (config) {
+    // Do something before request is sent
+    if (config.data && config.headers[config.method]['Content-Type'] === 'application/x-www-form-urlencoded') {
+      config.data = qs.stringify(config.data)
+    }
+    return config
+  }, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  })
 
   // all injected var can be used by this.${varname} in Vue and ${varname} in Nuxt, e.g. this.$http (Vue), $http (Nuxt)
-  inject('http', axios)
-  inject('qs', qs)
+  // inject('http', axios)
+  inject('http', $axios)
   inject('log', console.log.bind(console))
   inject('error', console.error.bind(console))
   inject('warn', console.warn.bind(console))
