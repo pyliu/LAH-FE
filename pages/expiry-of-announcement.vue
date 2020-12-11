792e3caf36fa03d0e4271c624a3b96cecaa64f06
pyliu <3634168+pyliu@users.noreply.github.com>
2020-12-11 19:35:47 +0800

    add basic flow for the rm30 H case

--- pages/expiry-of-announcement.vue
+++ pages/expiry-of-announcement.vue
@@ -1,23 +1,54 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo class="anim-appear-2s" />
-      <h3 class="title">
+  <div>
+    <lah-transition appear>
+      <h3 class="d-flex justify-content-between page-header padding-override">
         公告期滿案件
       </h3>
-      <div>
-        BBB
-      </div>
-    </div>
+    </lah-transition>
+    <lah-transition fade>
+      <lah-reg-b-table :baked-data="bakedData"></lah-reg-b-table>
+    </lah-transition>
+    <lah-transition class="center h3">
+      <lah-fa-icon
+        v-cloak
+        v-if="queryCount === 0 && committed"
+        action="bounce"
+        icon="yahoo"
+        prefix="fab"
+      >
+        無資料</lah-fa-icon
+      >
+    </lah-transition>
   </div>
 </template>
 
 <script>
 export default {
   head: {
-    title: '公告期滿案件-桃園市地政局'
+    title: "公告期滿案件-桃園市地政局",
+  },
+  data: () => ({
+    bakedData: [],
+    committed: false
+  }),
+  computed: {
+    queryCount () { return this.bakedData.length }
+  },
+  mounted () {
+    this.isBusy = true
+    this.$axios.post(this.$consts.API.JSON.QUERY, {
+      type: 'reg_rm30_H_case'
+    }).then((res) => {
+      this.bakedData = res.data.baked
+      this.notify(res.data.message)
+    }).catch(err => {
+      this.alert(err.message)
+      this.$utils.error(err)
+    }).finally(() => {
+      this.isBusy = false
+    })
   }
-}
+};
 </script>
 
 <style>
