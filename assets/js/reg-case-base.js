23ef30d582aafd95b4f9d26e13a73767996e5509
pyliu <3634168+pyliu@users.noreply.github.com>
2020-12-18 10:06:52 +0800

    add computed id

--- assets/js/reg-case-base.js
+++ assets/js/reg-case-base.js
@@ -15,46 +15,27 @@ export default {
     bakedData: undefined
   }),
   computed: {
-    year () {
-      return this.caseId.replace(/[^a-zA-Z0-9]/g, '').substring(0, 3)
-    },
-    code () {
-      return this.caseId.replace(/[^a-zA-Z0-9]/g, '').substring(3, 7)
-    },
-    number () {
-      return this.caseId.replace(/[^a-zA-Z0-9]/g, '').substring(7)
-    },
-    ID () {
-      return `${this.year}-${this.code}-${this.number.padStart(6, '0')}`
-    },
-    validID () {
-      return this.year.length === 3 && this.code.length === 4 && parseInt(this.number) < 1000000 && parseInt(this.number) > 0
-    },
-    ready () {
-      return !this.$utils.empty(this.bakedData)
-    },
-    queryDataUrl () {
-      return `http://${this.$consts.AP}:9080/Land${this.site}/CAS/CCD01/CCD0103.jsp?rm01=${this.bakedData['RM01']}&rm02=${this.bakedData['RM02']}&rm03=${this.bakedData['RM03']}`
-    },
-    queryStatusUrl () {
-      return `http://${this.$consts.AP}:9080/Land${this.site}/CAS/CCD02/CCD0202.jsp?year=${this.bakedData['RM01']}&word=${this.bakedData['RM02']}&code=${this.bakedData['RM03']}&sdlyn=N&RM90=`
-    }
+    year () { return this.caseId.replace(/[^a-zA-Z0-9]/g, '').substring(0, 3) },
+    code () { return this.caseId.replace(/[^a-zA-Z0-9]/g, '').substring(3, 7) },
+    number () { return this.caseId.replace(/[^a-zA-Z0-9]/g, '').substring(7) },
+    id () { return this.ID.replace('-', '') },
+    ID () { return `${this.year}-${this.code}-${this.number.padStart(6, '0')}` },
+    validID () { return this.year.length === 3 && this.code.length === 4 && parseInt(this.number) < 1000000 && parseInt(this.number) > 0 },
+    ready () { return !this.$utils.empty(this.bakedData) },
+    queryDataUrl () { return `http://${this.$consts.AP}:9080/Land${this.site}/CAS/CCD01/CCD0103.jsp?rm01=${this.bakedData['RM01']}&rm02=${this.bakedData['RM02']}&rm03=${this.bakedData['RM03']}` },
+    queryStatusUrl () { return `http://${this.$consts.AP}:9080/Land${this.site}/CAS/CCD02/CCD0202.jsp?year=${this.bakedData['RM01']}&word=${this.bakedData['RM02']}&code=${this.bakedData['RM03']}&sdlyn=N&RM90=` }
   },
   watch: {
-    parentData(val) {
-      this.bakedData = val
-    }
+    parentData(val) { this.bakedData = val }
   },
-  created () { 
-    this.bakedData = this.parentData
-  },
+  created () { this.bakedData = this.parentData },
   async fetch () {
+    // case-id prop-in is valid then uses it for fetching data
     if (this.validID) {
-      const thisID = `${this.year}${this.code}${this.number.padStart(6, '0')}`
       this.isBusy = true
       this.$axios.post(this.$consts.API.JSON.QUERY, {
         type: 'reg_case',
-        id: thisID
+        id: this.id
       }).then(res => {
         if (this.$utils.statusCheck(res.data.status)) {
           this.bakedData = res.data.baked
