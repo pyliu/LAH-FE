5f598bb401c099db53b310b7087206ee0df64525
pyliu <3634168+pyliu@users.noreply.github.com>
2021-01-03 09:49:11 +0800

    make wording less chars

--- pages/index.vue
+++ pages/index.vue
@@ -20,7 +20,7 @@
             <lah-index-card-link :icon="['fas', 'user-astronaut']" to="/foreigner-case">外人地權案件</lah-index-card-link>
           </b-card-group>
           <b-card-group deck>
-            <lah-index-card-link :icon="['fas', 'border-all']" to="/agriculture-375-change">三七五租約標的異動</lah-index-card-link>
+            <lah-index-card-link :icon="['fas', 'border-all']" to="/agriculture-375-change">三七五租約異動</lah-index-card-link>
             <lah-index-card-link icon-variant="muted" action="damage">未辦標的註記異動</lah-index-card-link>
             <lah-index-card-link icon-variant="muted" action="damage">參考資訊檔標的異動通知書產製</lah-index-card-link>
             <lah-index-card-link icon-variant="muted" action="damage">逾期未駁回查詢</lah-index-card-link>
