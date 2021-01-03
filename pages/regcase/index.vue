57a5f19649c2513daa16ee5090541e9192c87fdb
pyliu <3634168+pyliu@users.noreply.github.com>
2021-01-03 11:26:17 +0800

    add lah-header

--- pages/regcase/index.vue
+++ pages/regcase/index.vue
@@ -1,33 +1,37 @@
 <template>
   <div>
-    <div class="d-flex justify-content-between padding-override">
+    <lah-header>
+      <div class="d-flex justify-content-between w-100">
         <b-input-group size="lg" class="text-nowrap mr-1" style="max-width: 350px">
-            <b-input-group-prepend is-text>日期</b-input-group-prepend>
+          <b-input-group-prepend is-text>日期</b-input-group-prepend>
+          <client-only>
             <b-form-datepicker
-                value-as-date
-                v-model="date_obj"
-                placeholder="請選擇日期"
-                :date-disabled-fn="dateDisabled"
-                :max="new Date()"
+              value-as-date
+              v-model="date_obj"
+              placeholder="請選擇日期"
+              :date-disabled-fn="dateDisabled"
+              :max="new Date()"
             ></b-form-datepicker>
-            <b-button class="ml-1" @click.stop="reload()" variant="outline-primary" title="依據日期"><i class="fas fa-search"></i> 查詢</b-button>
+          </client-only>
+          <b-button class="ml-1" @click.stop="reload()" variant="outline-primary" title="依據日期"><i class="fas fa-search"></i> 查詢</b-button>
         </b-input-group>
         <b-input-group size="lg" class="text-nowrap my-auto">
-            <b-button variant="light" @click="$('.b-table tbody tr').removeClass('hide')" class="border px-3"><lah-fa-icon icon="list"></lah-fa-icon> 全部</b-button>
-            <b-button variant="success" @click="$('.b-table tbody tr').addClass('hide'); $('.b-table tbody tr.filter-success').removeClass('hide');"><lah-fa-icon icon="suitcase"></lah-fa-icon> 已結案</b-button>
-            <b-button variant="warning" @click="$('.b-table tbody tr').addClass('hide'); $('.b-table tbody tr.filter-warning').removeClass('hide');"><lah-fa-icon prefix="far" icon="clock"></lah-fa-icon> 快逾期</b-button>
-            <b-button variant="danger" @click="$('.b-table tbody tr').addClass('hide'); $('.b-table tbody tr.filter-danger').removeClass('hide');"><lah-fa-icon icon="stopwatch"></lah-fa-icon> 已逾期</b-button>
-            <b-button id="reload" variant="primary" @click="reload">
-                <lah-fa-icon icon="sync-alt"> 刷新</lah-fa-icon>
-                <b-badge variant="light">
-                    <countdown ref="countdown" :time="milliseconds" @end="handleCountdownEnd" @start="handleCountdownStart" :auto-start="false">
-                        <template slot-scope="props">{{ props.minutes.toString().padStart(2, '0') }}:{{ props.seconds.toString().padStart(2, '0') }}</template>
-                    </countdown>
-                    <span class="sr-only">倒數</span>
-                </b-badge>
-            </b-button>
+          <b-button variant="light" @click="$('.b-table tbody tr').removeClass('hide')" class="border px-3"><lah-fa-icon icon="list"></lah-fa-icon> 全部</b-button>
+          <b-button variant="success" @click="$('.b-table tbody tr').addClass('hide'); $('.b-table tbody tr.filter-success').removeClass('hide');"><lah-fa-icon icon="suitcase"></lah-fa-icon> 已結案</b-button>
+          <b-button variant="warning" @click="$('.b-table tbody tr').addClass('hide'); $('.b-table tbody tr.filter-warning').removeClass('hide');"><lah-fa-icon prefix="far" icon="clock"></lah-fa-icon> 快逾期</b-button>
+          <b-button variant="danger" @click="$('.b-table tbody tr').addClass('hide'); $('.b-table tbody tr.filter-danger').removeClass('hide');"><lah-fa-icon icon="stopwatch"></lah-fa-icon> 已逾期</b-button>
+          <b-button id="reload" variant="primary" @click="reload">
+            <lah-fa-icon icon="sync-alt"> 刷新</lah-fa-icon>
+            <b-badge variant="light">
+              <countdown ref="countdown" :time="milliseconds" @end="handleCountdownEnd" @start="handleCountdownStart" :auto-start="false">
+                <template slot-scope="props">{{ props.minutes.toString().padStart(2, '0') }}:{{ props.seconds.toString().padStart(2, '0') }}</template>
+              </countdown>
+              <span class="sr-only">倒數</span>
+            </b-badge>
+          </b-button>
         </b-input-group>
-    </div>
+      </div>
+    </lah-header>
     
     <b-modal
       :id="modalId"
@@ -70,7 +74,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.padding-override {
-  padding: .6rem 4rem;
-}
 </style>
