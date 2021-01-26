b28d388804909706850bf41c96587c96d5c27956
pyliu <3634168+pyliu@users.noreply.github.com>
2021-01-26 22:48:44 +0800

    add login page for admin

---
+++ pages/login.vue
@@ -0,0 +1,46 @@
+<template>
+  <div v-cloak>
+    <lah-header>
+      <lah-transition appear>
+        <div class="d-flex justify-content-between w-100">
+          <div class="d-flex">
+            <div class="my-auto">管理者登入</div>
+            <lah-button icon="question" variant="outline-success" no-border no-icon-gutter v-b-modal.help-modal title="說明"/>
+          </div>
+          <div></div>
+        </div>
+      </lah-transition>
+      <lah-help-modal :modal-id="'help-modal'">
+        請輸入主密碼登入為管理者
+      </lah-help-modal>
+    </lah-header>
+    <b-container fluid v-cloak>
+      <div class="vp-100 center">
+        <b-form-group class="center-50">
+            <b-form-input type="password" size="lg" class="text-center" trim></b-form-input>
+            <lah-button icon="sign-in-alt" size="lg" action="move-fade-ltr" class="mx-auto my-2">登入</lah-button>
+        </b-form-group>
+      </div>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  head: {
+    title: '管理者登入-桃園市地政局'
+  },
+  fetchOnServer: false
+}
+</script>
+
+<style lang="scss" scoped>
+.vp-100 {
+    width: 100%;
+    height: 65vh;
+}
+.center-50 {
+    width: 50%;
+    text-align: center;
+}
+</style>
