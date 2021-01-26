b90fd1063680be384aa00a9545934bbb6c928ac3
pyliu <3634168+pyliu@users.noreply.github.com>
2021-01-26 23:19:18 +0800

    Update login.vue
    add basic auth skeleton

--- pages/login.vue
+++ pages/login.vue
@@ -17,8 +17,8 @@
     <b-container fluid v-cloak>
       <div class="vp-100 center">
         <b-form-group class="center-50">
-            <b-form-input type="password" size="lg" class="text-center" trim></b-form-input>
-            <lah-button icon="sign-in-alt" size="lg" action="move-fade-ltr" class="mx-auto my-2">登入</lah-button>
+            <b-form-input v-model="password" type="password" size="lg" class="text-center" trim></b-form-input>
+            <lah-button @click="check" icon="sign-in-alt" size="lg" action="move-fade-ltr" class="mx-auto my-2">登入</lah-button>
         </b-form-group>
       </div>
     </b-container>
@@ -30,7 +30,19 @@ export default {
   head: {
     title: '管理者登入-桃園市地政局'
   },
-  fetchOnServer: false
+  data: () => ({
+    password: undefined,
+    hashedSecret: '4535972a186590f9fe2a470e02c9f22e'
+  }),
+  method: {
+    check () {
+      if (this.$utils.equal(this.$utils.md5(this.password), this.hashedSecret)) {
+        // ok
+      } else {
+        // not ok
+      }
+    }
+  }
 }
 </script>
 
