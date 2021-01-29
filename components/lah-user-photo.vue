3e26402347b31a1585b2e47cbc31e82df01a6d3e
pyliu <3634168+pyliu@users.noreply.github.com>
2021-01-29 11:45:33 +0800

    add id / name props

--- components/lah-user-photo.vue
+++ components/lah-user-photo.vue
@@ -11,13 +11,21 @@
 <script>
 export default {
   props: {
-    userData: { type: Object, required: true },
+    userData: { type: Object, default: () => ({}) },
+    id: { type: String, default: '' },
+    name: { type: String, default: '' },
     fluid: { type: Boolean, default: true },
     thumbnail: { type: Boolean, default: false }
   },
   computed: {
+    userId () {
+      return this.userData['id'] || this.id
+    },
+    userName () {
+      return this.userData['name'] || this.name
+    },
     src () {
-      return `${this.apiSvrHttpUrl}/get_user_img.php?id=${this.userData['id']}&name=${this.userData['name']}`
+      return `${this.apiSvrHttpUrl}/get_user_img.php?id=${this.userId}&name=${this.userName}`
     }
   }
 }
