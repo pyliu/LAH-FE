9635e0cc284e4734884e55d94bd7b53b00eb9f28
pyliu <3634168+pyliu@users.noreply.github.com>
2021-02-03 16:25:14 +0800

    comment

--- middleware/expireAuth.js
+++ middleware/expireAuth.js
@@ -2,11 +2,12 @@ export default function ({
     store,
     redirect
   }) {
+    // auth info not ready redirect to /
     if (store.state.svr === null) {
       return redirect('/')
     }
     const authority = store.state.svr.config.authority
-    // not admin => redirect to '/login'
+    // not chief => redirect to personal page
     if (!authority.isChief && !authority.isAdmin && !authority.isSuper) {
       const myinfo = store.state.svr.user
       return redirect(`/expire/${myinfo.name}%20${myinfo.id}`)
