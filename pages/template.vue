1d03d7fbfdfbd925bc6d7f4421f23fe1d3ab02b3
pyliu <3634168+pyliu@users.noreply.github.com>
2021-02-09 14:04:01 +0800

    pug

--- pages/template.vue
+++ pages/template.vue
@@ -1,30 +1,15 @@
-<template>
-  <div v-cloak>
-    <lah-header>
-      <lah-transition appear>
-        <div class="d-flex justify-content-between w-100 my-auto">
-          <div class="d-flex">
-            <div>標題區域</div>
-            <lah-button icon="question" variant="outline-success" no-border no-icon-gutter v-b-modal.help-modal title="說明"/>
-          </div>
-          <div>右側選單區域</div>
-        </div>
-      </lah-transition>
-      <lah-help-modal :modal-id="'help-modal'" size="xl">
-        <lah-button icon="exclamation-circle" variant="danger"></lah-button>
-      </lah-help-modal>
-    </lah-header>
-    <b-container fluid v-cloak class="center">
-      <div>
-        <h3 class="title">
-          樣板
-        </h3>
-        <div>
-          some text goes here ...
-        </div>
-      </div>
-    </b-container>
-  </div>
+<template lang="pug">
+  div(v-cloak)
+    lah-header
+      lah-transition(appear)
+        .d-flex.justify-content-between.w-100.my-auto
+          .d-flex
+            div 標題區域
+            lah-button(icon="question" variant="outline-success" no-border no-icon-gutter v-b-modal.help-modal title="說明")
+          div 右側選單區域
+      lah-help-modal(:modal-id="'help-modal'" size="xl"): lah-button(icon="exclamation-circle" variant="danger")
+    b-container.center(fluid v-cloak)
+      | some content goes here ...
 </template>
 
 <script>
