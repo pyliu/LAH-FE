5ed9fbafbcb3e79ea1b65cd8e31230455897206b
pyliu <3634168+pyliu@users.noreply.github.com>
2021-02-09 16:16:33 +0800

    pug

--- components/lah-help-modal.vue
+++ components/lah-help-modal.vue
@@ -1,23 +1,39 @@
-<template>
-  <b-modal
+<template lang="pug">
+  b-modal(
     :id="modalId"
     :hide-footer="hideFooter"
     :centered="centered"
     :size="size"
     :scrollable="scrollable"
     :no-close-on-backdrop="noCloseOnBackdrop"
-  >
-    <template #modal-title>
-      <div v-html="modalTitle"></div>
-    </template>
-    <slot></slot>
-    <hr/>
-    <p class="h6 my-2 align-middle">滑鼠點擊 <lah-fa-icon icon="share-square" variant="primary"/>開啟案件詳情視窗</p>
-    <p class="h6 my-2 align-middle">滑鼠點擊 <lah-fa-icon icon="sync" variant="secondary"/>立即讀取最新資料</p>
-    <p class="h6 my-2 align-middle">滑鼠點擊 <lah-fa-icon icon="bars" variant="dark"/>顯示側邊攔選單</p>
-    <p class="h6 my-2 align-middle">滑鼠點擊 <lah-fa-icon icon="question" variant="success"/>查看頁面說明</p>
-    <p class="h6 my-2 align-middle">滑鼠點擊 <lah-fa-icon icon="home" variant="dark"/>回首頁</p>
-  </b-modal>
+  )
+    template(#modal-title): div(v-html="modalTitle")
+    slot
+    hr
+    p.h6.my-2.align-middle.
+      滑鼠點擊
+      #[lah-fa-icon(icon="share-square" variant="primary")]
+      開啟案件詳情視窗
+    
+    p.h6.my-2.align-middle.
+      滑鼠點擊
+      #[lah-fa-icon(icon="sync" variant="secondary")]
+      立即讀取最新資料
+    
+    p.h6.my-2.align-middle.
+      滑鼠點擊
+      #[lah-fa-icon(icon="bars" variant="dark")]
+      顯示側邊攔選單
+    
+    p.h6.my-2.align-middle.
+      滑鼠點擊
+      #[lah-fa-icon(icon="question" variant="success")]
+      查看頁面說明
+    
+    p.h6.my-2.align-middle.
+      滑鼠點擊
+      #[lah-fa-icon(icon="home" variant="dark")]
+      回首頁
 </template>
 
 <script>
