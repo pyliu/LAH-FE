558dab3e6af87cd926589236ad9b0e1d6cef3d73
pyliu <3634168+pyliu@users.noreply.github.com>
2021-02-25 17:34:45 +0800

    add scripts to control pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,6 @@ module.exports = {
     instances: -1,
     script: './node_modules/nuxt/bin/nuxt.js',
     args: 'start',
-    error_file: 'C:/AppServ/www/log',
-    out_file: 'C:/AppServ/www/log'
+    cron: '0 7 * * *'
   }]
 }
