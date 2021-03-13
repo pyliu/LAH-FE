ba5ae5c9fbb6c1f35ad2db0e3dcd70b779ebdf55
pyliu <3634168+pyliu@users.noreply.github.com>
2021-03-13 16:15:04 +0800

    add msToHuman in $utils

--- pages/foreigner-case.vue
+++ pages/foreigner-case.vue
@@ -6,16 +6,18 @@
           .my-auto 外國人地權案件
           lah-button(icon="question" action="bounce" variant="outline-success" no-border no-icon-gutter @click="showModalById('help-modal')" title="說明")
           lah-help-modal(:modal-id="'help-modal'")
-            .h5 利用下面介面操作資料搜尋時間：
-            .d-flex.my-2.text-nowrap
-              b-form-input.my-auto(type="range" v-model="rangeStep" min="1" max="24")
-              span.my-auto.mr-2 {{ year }}-{{ month }}
+            .h5 請利用日期區間搜尋
         .d-flex
           lah-datepicker.mr-1(v-model="dateRange")
-          //- b-input-group.vw-50.mr-2(size="sm")
-          //-   template(#prepend): b-input-group-text: lah-fa-icon.font-weight-bold(icon="calendar" regular).
-          //-     {{year}}年{{month}}月
-          //-   b-form-input.my-auto(type="range" v-model="rangeStep" min="1" max="24")
+          lah-button.mr-1(
+            ref="search"
+            icon="search"
+            size="lg"
+            title="搜尋"
+            :disabled="isBusy || isWrongDaysPeriod"
+            @click="$fetch"
+            no-icon-gutter
+          )
           lah-countdown-button(
             ref="countdown"
             icon="sync-alt"
@@ -52,147 +54,130 @@ export default {
     title: '外國人地權案件-桃園市地政局'
   },
   fetchOnServer: false,
-  data: () => {
-    const now = new Date()
-    let month = now.getMonth()
-    let year = now.getFullYear() - 1911
-    month === 0 && (month = 12, year--)
-    // set last month as default
-    const defMonth = ("0" + month).slice(-2)
-    const rangeBase = (now.getFullYear() - 1911) * 12 + now.getMonth() + 1
-    return {
-      forceReload: false,
-      dateRange: {
-        beigh: '',
-        end: '',
-        days: 0
+  data: () => ({
+    forceReload: false,
+    committed: false,
+    dateRange: {
+      beigh: '',
+      end: '',
+      days: 0
+    },
+    cachedMs: 24 * 60 * 60 * 1000,
+    bakedData: [],
+    fields: [
+      {
+        key: '收件字號',
+        sortable: true
       },
-      year: year,
-      month: defMonth,
-      cachedMs: 24 * 60 * 60 * 1000,
-      committed: false,
-      rangeMax: 24,
-      rangeStep: 23,
-      rangeBase: rangeBase,
-      bakedData: [],
-      fields: [
-        {
-          key: '收件字號',
-          sortable: true
-        },
-        {
-          key: '登記原因',
-          sortable: true
-        },
-        {
-          key: '收件日期',
-          sortable: true
-        },
-        {
-          key: '結案日期',
-          sortable: true
-        },
-        {
-          key: '權利人統一編號',
-          sortable: true
-        },
-        {
-          key: '權利人姓名',
-          sortable: true
-        },
-        {
-          key: '義務人統一編號',
-          sortable: true
-        },
-        {
-          key: '義務人姓名',
-          sortable: true
-        },
-        {
-          key: '外國人類別',
-          sortable: true
-        },
-        {
-          key: '辦理情形',
-          sortable: true
-        },
-        {
-          key: '結案與否',
-          sortable: true
-        }
-      ]
-    }
-  },
+      {
+        key: '登記原因',
+        sortable: true
+      },
+      {
+        key: '收件日期',
+        sortable: true
+      },
+      {
+        key: '校對日期',
+        sortable: true
+      },
+      {
+        key: '權利人統一編號',
+        sortable: true
+      },
+      {
+        key: '權利人姓名',
+        sortable: true
+      },
+      {
+        key: '義務人統一編號',
+        sortable: true
+      },
+      {
+        key: '義務人姓名',
+        sortable: true
+      },
+      {
+        key: '外國人類別',
+        sortable: true
+      },
+      {
+        key: '辦理情形',
+        sortable: true
+      },
+      {
+        key: '結案與否',
+        sortable: true
+      }
+    ]
+  }),
   computed: {
-    cacheKey () { return `foreigner-case-${this.year}${this.month}` }
-  },
-  watch: {
-    rangeStep (val) {
-      const after = this.rangeBase - this.rangeMax + parseInt(val) - 1
-      this.year = parseInt(after / 12)
-      this.month = ("0" + (after % 12 + 1)).slice(-2)
-      this.reloadDebounced()
-    }
+    cacheKey () { return `foreigner-case-${this.dateRange.begin}-${this.dateRange.end}` },
+    isWrongDaysPeriod () { return this.dateRange.days < 1 }
   },
   methods: {
-    reloadDebounced () {/* placeholder */},
     reload () {
       this.forceReload = true
       this.$fetch()
     }
   },
   fetch () {
-    // restore cached data if found
-    this.getCache(this.cacheKey).then(json => {
-      if (json === false || this.forceReload) {
-        if(this.isBusy) {
-          this.notify('讀取中 ... 請稍後', { type: 'warning' })
-        } else {
-          this.isBusy = true
-          this.committed = false
-          this.$axios.post(this.$consts.API.JSON.PREFETCH, {
-            type: `reg_foreigner_case`,
-            year_month: `${this.year}${this.month}`,
-            reload: this.forceReload
-          }).then(({ data }) => {
-            this.bakedData = data.baked || []
-            this.notify(data.message, {
-              type: this.$utils.statusCheck(data.status) ? 'info' : 'warning',
-              subtitle: `${this.year}-${this.month}`
-            })
-            const remain_s = data.cache_remaining_time // in seconds
-            const remain_ms = remain_s * 1000
-            if (remain_ms && remain_ms > 0) {
-              this.setCache(this.cacheKey, data, remain_ms)
-              if (this.$refs.countdown) {
-                this.$refs.countdown.setCountdown(remain_ms)
-                this.$refs.countdown.startCountdown()
+    if(this.isBusy) {
+      this.notify('讀取中 ... 請稍後', { type: 'warning' })
+    } else if (this.$utils.empty(this.dateRange.begin) || this.$utils.empty(this.dateRange.end)) {
+        this.$utils.warn('dateRange is not ready ... postpone $fetch')
+        this.timeout(this.$fetch, 250)
+    } else {
+      // restore cached data if found
+      this.getCache(this.cacheKey).then(json => {
+        if (json === false || this.forceReload) {
+          if(this.isBusy) {
+            this.notify('讀取中 ... 請稍後', { type: 'warning' })
+          } else {
+            this.isBusy = true
+            this.committed = false
+            this.$axios.post(this.$consts.API.JSON.PREFETCH, {
+              type: `reg_foreigner_case`,
+              begin: this.dateRange.begin,
+              end: this.dateRange.end,
+              reload: this.forceReload
+            }).then(({ data }) => {
+              this.bakedData = data.baked || []
+              this.notify(data.message, {
+                type: this.$utils.statusCheck(data.status) ? 'info' : 'warning',
+                subtitle: `${this.dateRange.begin}-${this.dateRange.end}`
+              })
+              const remain_s = data.cache_remaining_time // in seconds
+              const remain_ms = remain_s * 1000
+              if (remain_ms && remain_ms > 0) {
+                this.setCache(this.cacheKey, data, remain_ms)
+                if (this.$refs.countdown) {
+                  this.$refs.countdown.setCountdown(remain_ms)
+                  this.$refs.countdown.startCountdown()
+                }
               }
+            }).catch(err => {
+              this.alert(err.message)
+              this.$utils.error(err)
+            }).finally(() => {
+              this.isBusy = false
+              this.forceReload = false
+              this.committed = true
+            })
+          }
+        } else {
+          this.bakedData = json.baked
+          this.committed = true
+          this.getCacheExpireRemainingTime(this.cacheKey).then(remaining => {
+            if (this.$refs.countdown) {
+              this.$refs.countdown.setCountdown(remaining)
+              this.$refs.countdown.startCountdown()
             }
-          }).catch(err => {
-            this.alert(err.message)
-            this.$utils.error(err)
-          }).finally(() => {
-            this.isBusy = false
-            this.forceReload = false
-            this.committed = true
+            this.notify(`查詢成功，找到 ${this.bakedData.length} 筆外國人地權案件。`, { subtitle: `(快取) ${this.$utils.msToHuman(remaining)} 後更新` })
           })
         }
-      } else {
-        this.bakedData = json.baked
-        this.committed = true
-        this.notify(`查詢成功，找到 ${this.bakedData.length} 筆外國人地權案件。`, { subtitle: `${this.cacheKey}(快取)` })
-        this.getCacheExpireRemainingTime(this.cacheKey).then(remaining => {
-          if (this.$refs.countdown) {
-            this.$refs.countdown.setCountdown(remaining)
-            this.$refs.countdown.startCountdown()
-          }
-        })
-      }
-    })
-  },
-  created () {
-    this.reloadDebounced = this.$utils.debounce(this.$fetch, 1000)
+      })
+    }
   }
 }
 </script>
--- plugins/global-init.js
+++ plugins/global-init.js
@@ -288,6 +288,21 @@ export default ({ $axios, store }, inject) => {
       }
       return txt
     },
+    msToHuman(remain_ms) {
+      const seconds = (remain_ms / 1000).toFixed(1);
+      const minutes = (remain_ms / (1000 * 60)).toFixed(1);
+      const hours = (remain_ms / (1000 * 60 * 60)).toFixed(1);
+      const days = (remain_ms / (1000 * 60 * 60 * 24)).toFixed(1);
+      if (seconds < 60) {
+        return seconds + " 秒";
+      } else if (minutes < 60) {
+        return minutes + " 分";
+      } else if (hours < 24) {
+        return hours + " 小時";
+      } else {
+        return days + " 天"
+      }
+    },
     log: console.log.bind(console),
     warn: console.warn.bind(console),
     assert: console.assert.bind(console),
