a9bec4ab1f5ee0a7b92bbae0768fb1937f5fa943
pyliu <3634168+pyliu@users.noreply.github.com>
2021-04-16 15:46:14 +0800

    add uuid method in mixin

--- plugins/global-vue-mixin.js
+++ plugins/global-vue-mixin.js
@@ -96,6 +96,17 @@ Vue.mixin({
       'login'
     ]),
     $,  // jQuery '$',
+    uuid: function() {
+      var d = Date.now();
+      if (typeof performance !== 'undefined' && typeof performance.now === 'function'){
+        d += performance.now() //use high-precision timer if available
+      }
+      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+      })
+    },
     parseHTML (string) {
       const context = document.implementation.createHTMLDocument()
       // Set the base href for the created document so any parsed elements with URLs
