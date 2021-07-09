a867daee4f56436010bb01458552a75b0dc9c087
pyliu <3634168+pyliu@users.noreply.github.com>
2021-07-09 15:39:10 +0800

    wording

--- pages/admin/users/index.vue
+++ pages/admin/users/index.vue
@@ -73,7 +73,7 @@
                 class="mx-1 mt-n1"
                 size="sm"
               >
-                {{ site }}ＯＯＯＯ 中壢地政
+                {{ site }}ＯＯＯＯ 桃園地政
               </b-button>
               編輯使用者。
             </div>
