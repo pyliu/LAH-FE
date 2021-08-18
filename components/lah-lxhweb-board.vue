f7ca57b078f54fe40da661dd66e0e05ec7ac7d0f
pyliu <3634168+pyliu@users.noreply.github.com>
2021-08-18 21:35:46 +0800

    add ip api

--- components/lah-lxhweb-board.vue
+++ components/lah-lxhweb-board.vue
@@ -1,6 +1,6 @@
 <template>
   <b-card border-variant="secondary" class="shadow">
-    <template v-slot:header>
+    <template #header>
       <div class="d-flex w-100 justify-content-between">
         <h6 class="my-auto font-weight-bolder">
           <lah-fa-icon icon="traffic-light" size="lg" :variant="headerLight">
@@ -34,11 +34,11 @@
       <div class="offices">
         <div v-for="entry in offices" :key="entry.SITE" class="office center">
           <lah-fa-icon
+            v-b-popover.hover.focus.top="'最後更新時間: ' + entry.UPDATE_DATETIME"
             size="lg"
             icon="circle"
             :variant="light(entry)"
             :action="action(entry)"
-            v-b-popover.hover.focus.top="'最後更新時間: ' + entry.UPDATE_DATETIME"
           >
             {{ name(entry) }}
           </lah-fa-icon>
@@ -74,13 +74,38 @@ export default {
     pingTimer: null,
     brokenTableRaw: []
   }),
+  fetch () {
+    this.$axios.post(this.$consts.API.JSON.LXHWEB, {
+      type: 'lxhweb_config'
+    }).then(({ data }) => {
+      if (this.$utils.statusCheck(data.status)) {
+        this.configs = Object.assign(this.configs, data.raw)
+      } else {
+        this.$utils.error(data.message)
+      }
+    }).catch((err) => {
+      this.$utils.error(err)
+    }).finally(() => {
+    })
+    // mock data
+    this.offices = [
+      { SITE: 'HA', UPDATE_DATETIME: this.randDate() },
+      { SITE: 'HB', UPDATE_DATETIME: this.randDate() },
+      { SITE: 'HC', UPDATE_DATETIME: this.randDate() },
+      { SITE: 'HD', UPDATE_DATETIME: this.randDate() },
+      { SITE: 'HE', UPDATE_DATETIME: this.randDate() },
+      { SITE: 'HF', UPDATE_DATETIME: this.randDate() },
+      { SITE: 'HG', UPDATE_DATETIME: this.randDate() },
+      { SITE: 'HH', UPDATE_DATETIME: this.randDate() }
+    ]
+  },
   computed: {
     ip () {
       return this.hwebMap.get(this.targetIp).ip
     },
     header () {
       if (this.hwebMap.has(this.targetIp)) {
-        let entry = this.hwebMap.get(this.targetIp)
+        const entry = this.hwebMap.get(this.targetIp)
         return `${entry.name} ${entry.ip}`
       }
       return `未支援 ${this.targetIp} 監控`
@@ -91,15 +116,15 @@ export default {
     },
     headerLight () {
       if (this.alive) {
-        let site_light = "success"
+        let site_light = 'success'
         for (let i = 0; i < this.offices.length; i++) {
-          let this_light = this.light(this.offices[i])
-          if (this_light == "warning") site_light = "warning"
-          if (this_light == "danger") return "danger"
+          const this_light = this.light(this.offices[i])
+          if (this_light == 'warning') { site_light = 'warning' }
+          if (this_light == 'danger') { return 'danger' }
         }
         return site_light
       }
-      return "secondary"
+      return 'secondary'
     },
     brokenTableCount () {
       return this.$utils.empty(this.brokenTableRaw) ? 0 : this.brokenTableRaw.length
@@ -109,69 +134,74 @@ export default {
     },
     hwebMap () {
       return new Map([
-        ["L1HWEB", { name: "L1同步異動", code: "L1HWEB", ip: this.l1hwebIp }],
-        ["L1HWEB_Alt", { name: "L1同步異動(備)", code: "L1HWEB_Alt", ip: this.l1hwebAltIp }],
-        ["L2HWEB", { name: "L2同步異動", code: "L2HWEB", ip: this.l2hwebIp }],
-        ["L3HWEB", { name: "L3同步異動", code: "L3HWEB", ip: this.l3hwebIp }]
+        ['L1HWEB', { name: 'L1同步異動', code: 'L1HWEB', ip: this.l1hwebIp }],
+        ['L1HWEB_Alt', { name: 'L1同步異動(備)', code: 'L1HWEB_Alt', ip: this.l1hwebAltIp }],
+        ['L2HWEB', { name: 'L2同步異動', code: 'L2HWEB', ip: this.l2hwebIp }],
+        ['L3HWEB', { name: 'L3同步異動', code: 'L3HWEB', ip: this.l3hwebIp }]
       ])
     },
     l1hwebIp () {
-      return this.$utils.empty(this.configs) ? '220.1.33.2' : this.configs['ORA_DB_L1HWEB_IP']
+      return this.$utils.empty(this.configs) ? '220.1.33.2' : this.configs.ORA_DB_L1HWEB_IP
     },
     l1hwebAltIp () {
       return this.l3hwebIp
     },
     l2hwebIp () {
-      return this.$utils.empty(this.configs) ? '220.1.33.3' : this.configs['ORA_DB_L2HWEB_IP']
+      return this.$utils.empty(this.configs) ? '220.1.33.3' : this.configs.ORA_DB_L2HWEB_IP
     },
     l3hwebIp () {
-      return this.$utils.empty(this.configs) ?  '220.1.33.5' : this.configs['ORA_DB_L3HWEB_IP']
+      return this.$utils.empty(this.configs) ? '220.1.33.5' : this.configs.ORA_DB_L3HWEB_IP
     },
     pingInterval () {
-      return this.$utils.empty(this.configs) ? 5 * 60 * 1000 : parseInt(this.configs['PING_INTERVAL_SECONDS']) * 1000
+      return this.$utils.empty(this.configs) ? 5 * 60 * 1000 : parseInt(this.configs.PING_INTERVAL_SECONDS) * 1000
     }
+  },
+  mounted () {
+    this.ping()
+  },
+  beforeDestroy () {
+    this.stopPing()
   },
   methods: {
     randDate () {
-      let rand_date = new Date(+new Date() - this.$utils.rand(45 * 60 * 1000))
+      const rand_date = new Date(+new Date() - this.$utils.rand(45 * 60 * 1000))
       return (
         rand_date.getFullYear() +
-        "-" +
-        ("0" + (rand_date.getMonth() + 1)).slice(-2) +
-        "-" +
-        ("0" + rand_date.getDate()).slice(-2) +
-        " " +
-        ("0" + rand_date.getHours()).slice(-2) +
-        ":" +
-        ("0" + rand_date.getMinutes()).slice(-2) +
-        ":" +
-        ("0" + rand_date.getSeconds()).slice(-2)
+        '-' +
+        ('0' + (rand_date.getMonth() + 1)).slice(-2) +
+        '-' +
+        ('0' + rand_date.getDate()).slice(-2) +
+        ' ' +
+        ('0' + rand_date.getHours()).slice(-2) +
+        ':' +
+        ('0' + rand_date.getMinutes()).slice(-2) +
+        ':' +
+        ('0' + rand_date.getSeconds()).slice(-2)
       )
     },
     action (entry) {
-      let light = this.light(entry)
+      const light = this.light(entry)
       switch (light) {
-        case "danger":
-          return "tremble"
-        case "warning":
-          return "beat"
+        case 'danger':
+          return 'tremble'
+        case 'warning':
+          return 'beat'
         default:
-          return ""
+          return ''
       }
     },
     light (entry) {
       if (this.alive) {
         const now = +new Date() // in ms
-        const last_update = +new Date(entry.UPDATE_DATETIME.replace(" ", "T"))
-        let offset = now - last_update
-        if (offset > 30 * 60 * 1000) return "danger"
-        else if (offset > 15 * 60 * 1000) return "warning"
-        return "success"
+        const last_update = +new Date(entry.UPDATE_DATETIME.replace(' ', 'T'))
+        const offset = now - last_update
+        if (offset > 30 * 60 * 1000) { return 'danger' } else if (offset > 15 * 60 * 1000) { return 'warning' }
+        return 'success'
       }
-      return "secondary"
+      return 'secondary'
     },
     name (entry) {
-      for (var value of this.xapMap.values()) {
+      for (const value of this.xapMap.values()) {
         if (value.code == entry.SITE) {
           return value.name
         }
@@ -181,9 +211,9 @@ export default {
       this.stopPing()
       this.isBusy = true
       this.$axios.post(this.$consts.API.JSON.QUERY, {
-        type: "ping",
+        type: 'ping',
         ip: this.ip,
-        port: 1521, // db port
+        port: 1521 // db port
       }).then(({ data }) => {
         this.pingLatency = data.latency
         this.pingMessage = data.message
@@ -206,11 +236,11 @@ export default {
     stopPing () {
       clearTimeout(this.pingTimer)
     },
-    reload() {
+    reload () {
       if (this.alive) {
         this.$axios.post(this.$consts.API.JSON.LXHWEB, {
-          type: "lxhweb_site_update_time",
-          site: this.targetIp,
+          type: 'lxhweb_site_update_time',
+          site: this.targetIp
         }).then(({ data }) => {
           if (this.$utils.statusCheck(data.status)) {
             // array of {SITE: 'HB', UPDATE_DATETIME: '2020-10-08 21:47:00'}
@@ -234,14 +264,14 @@ export default {
     checkBrokenTable () {
       if (this.alive) {
         this.$axios.post(this.$consts.API.JSON.LXHWEB, {
-          type: "lxhweb_broken_table",
-          site: this.targetIp,
+          type: 'lxhweb_broken_table',
+          site: this.targetIp
         }).then(({ data }) => {
           if (data.data_count) {
             // found
             this.alert(`<i class="fa fa-exclamation-triangle fa-lg ld ld-beat"></i> 找到 ${data.data_count} 筆損毀表格`, {
               subtitle: this.targetIp,
-              title: "同步異動表格檢測",
+              title: '同步異動表格檢測',
               delay: 15 * 1000
             })
             this.brokenTableRaw = data.raw
@@ -257,52 +287,21 @@ export default {
     },
     showBrokenTable () {
       if (!this.$utils.empty(this.brokenTableRaw)) {
-        this.modal(this.$createElement("b-table", {
-            props: {
-              striped: true,
-              hover: true,
-              headVariant: "dark",
-              bordered: true,
-              captionTop: true,
-              caption: `找到 ${this.brokenTableCount} 件`,
-              items: this.brokenTableRaw,
-            },
-          }), {
-          title: "同步異動損毀表格"
+        this.modal(this.$createElement('b-table', {
+          props: {
+            striped: true,
+            hover: true,
+            headVariant: 'dark',
+            bordered: true,
+            captionTop: true,
+            caption: `找到 ${this.brokenTableCount} 件`,
+            items: this.brokenTableRaw
+          }
+        }), {
+          title: '同步異動損毀表格'
         })
       }
-    },
-  },
-  fetch () {
-    this.$axios.post(this.$consts.API.JSON.LXHWEB, {
-      type: "lxhweb_config",
-    }).then(({ data }) => {
-      if (this.$utils.statusCheck(data.status)) {
-        this.configs = Object.assign(this.configs, data.raw)
-      } else {
-        this.$utils.error(data.message)
-      }
-    }).catch((err) => {
-      this.$utils.error(err)
-    }).finally(() => {
-    })
-    // mock data
-    this.offices = [
-      { SITE: "HA", UPDATE_DATETIME: this.randDate() },
-      { SITE: "HB", UPDATE_DATETIME: this.randDate() },
-      { SITE: "HC", UPDATE_DATETIME: this.randDate() },
-      { SITE: "HD", UPDATE_DATETIME: this.randDate() },
-      { SITE: "HE", UPDATE_DATETIME: this.randDate() },
-      { SITE: "HF", UPDATE_DATETIME: this.randDate() },
-      { SITE: "HG", UPDATE_DATETIME: this.randDate() },
-      { SITE: "HH", UPDATE_DATETIME: this.randDate() }
-    ]
-  },
-  mounted () {
-    this.ping()
-  },
-  beforeDestroy () {
-    this.stopPing()
+    }
   }
 }
 </script>
