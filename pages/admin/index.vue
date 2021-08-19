c5c5b02a00c6a2f14d9dee95046ade2c2c8c0e6d
pyliu <3634168+pyliu@users.noreply.github.com>
2021-08-19 17:33:39 +0800

    reordering

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -11,20 +11,20 @@
         |請選擇下方功能區塊進行管理
         hr
         b-card-group(deck)
-          lah-index-card-link(:icon="['fas', 'users-cog']" to="/admin/users" no-body size="2x" icon-variant="primary" action="rubber-h" class="pt-2") 使用者資訊管理
-          lah-index-card-link(:icon="['fas', 'user-tag']" to="/admin/roles" no-body size="2x" icon-variant="success" action="wander-h" class="pt-2") 使用者角色管理
+          lah-index-card-link(:icon="['fab', 'watchman-monitoring']" to="/admin/lxhweb" no-body size="2x" icon-variant="info" action="flip-h" class="pt-2") 同步異動監控
+          lah-index-card-link(:icon="['fas', 'user-tag']" to="/admin/roles" no-body size="2x" icon-variant="success" action="wander-h" class="pt-2") IP角色管理
           lah-index-card-link(:icon="['fas', 'tasks']" to="/admin/configs" no-body size="2x" icon-variant="danger" action="rubber-v" class="pt-2") 系統參數設定
         b-card-group(deck style="margin-top: 15px;")
-          lah-index-card-link(:icon="['fab', 'watchman-monitoring']" to="/admin/lxhweb" no-body size="2x" icon-variant="info" action="measure" class="pt-2") 系統參數設定
-          b-card
-          b-card
+          lah-index-card-link(:icon="['fas', 'users-cog']" to="/admin/users" no-body size="2x" icon-variant="primary" action="rubber-h" class="pt-2") 使用者資訊管理
+          lah-index-card-link(:icon="['fas', 'chalkboard']" icon-variant="muted" action="damage" no-body size="2x" to="/stats" class="pt-2") 統計看板
+          lah-index-card-link(:icon="['fab', 'xbox']" icon-variant="muted" action="damage" no-body size="2x" to="/playground" class="pt-2") 測試
     b-container(fluid v-cloak)
       b-card-group(deck class="row")
-        lah-index-card-link(:icon="['fas', 'users-cog']" to="/admin/users" no-body size="5x" icon-variant="primary" action="rubber-h") 使用者資訊管理
-        lah-index-card-link(:icon="['fas', 'user-tag']" to="/admin/roles" no-body size="5x" icon-variant="success" action="wander-h") 使用者角色管理
+        lah-index-card-link(:icon="['fab', 'watchman-monitoring']" to="/admin/lxhweb" no-body size="5x" icon-variant="info" action="flip-h") 同步異動監控
+        lah-index-card-link(:icon="['fas', 'user-tag']" to="/admin/roles" no-body size="5x" icon-variant="success" action="wander-h") IP角色管理
         lah-index-card-link(:icon="['fas', 'tasks']" to="/admin/configs" no-body size="5x" icon-variant="danger" action="rubber-v") 系統參數設定
       b-card-group(deck class="row")
-        lah-index-card-link(:icon="['fab', 'watchman-monitoring']" to="/admin/lxhweb" no-body size="5x" icon-variant="info" action="flip-h") 同步異動監控
+        lah-index-card-link(:icon="['fas', 'users-cog']" to="/admin/users" no-body size="5x" icon-variant="primary" action="rubber-h") 使用者資訊管理
         lah-index-card-link(:icon="['fas', 'chalkboard']" icon-variant="muted" action="damage" no-body size="5x" to="/stats") 統計看板
         lah-index-card-link(:icon="['fab', 'xbox']" icon-variant="muted" action="damage" no-body size="5x" to="/playground") 測試
 </template>
