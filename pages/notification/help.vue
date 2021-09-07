6e58e22c4426f8baa681b20fbb22be04ba35c795
pyliu <3634168+pyliu@users.noreply.github.com>
2021-09-07 17:52:51 +0800

    redirect to index page when authority checking passed

--- pages/notification/help.vue
+++ pages/notification/help.vue
@@ -11,7 +11,7 @@
         //-   li 內容支援 Markdown 語法，請參考 #[a(href="https://bit.ly/mdcheat" target="_blank") #[b https://bit.ly/mdcheat]] 教學
       .d-flex
     b-card.center.text-center.vh-85
-      h1 您需要有公告管理者權限(如無請洽系統管理者調整)，確認後請按 #[nuxt-link(to="/notification") 此處] 進行新增傳送。
+      h1 您需要有權限才能發送所內公告，若有需求請洽系統管理者調整。
       h2 傳送個人訊息無權限限制，請至 #[nuxt-link(to="/message") 此頁面] 進行傳送。
       h3 PC端信差即時通請至 #[a(href="http://220.1.34.18:8888/service/section/%E8%B3%87%E8%A8%8A%E8%AA%B2/soft.html" target="_blank") 所內知識網] 下載安裝。
 </template>
@@ -20,13 +20,16 @@
 export default {
   data: () => ({}),
   head: {
-    title: '傳送公告訊息說明 - 桃園地政事務所'
+    title: '公告訊息說明 - 桃園地政事務所'
   },
   computed: {
   },
   watch: {
   },
   created () {
+    if (this.authority.isAdmin || this.authority.isNotifyMgtStaff) {
+      this.$router.push('/notification')
+    }
   },
   mounted () {
   },
