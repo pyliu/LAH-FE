251ee3cd90182d87ddac2d3f20839214423e5709
pyliu <3634168+pyliu@users.noreply.github.com>
2021-09-08 12:35:47 +0800

    help message

--- pages/admin/ip/index.vue
+++ pages/admin/ip/index.vue
@@ -7,7 +7,7 @@
         lah-help-modal(:modal-id="'help-modal'"): ol
           li 利用本頁面管理靜態IP對應表
           li 查看使用者回報IP紀錄
-            ul: li 電腦端需安裝 #[a(href="https://ppt.cc/fI3xYx" target="_blank") #[b 信差即時通程式(外網下載)]] 並正常連線才能正常回報IP
+            ul: li 電腦端需安裝 #[b 信差即時通程式] 並正常連線才能正常回報IP
       .d-flex
 
     b-card-group(deck)
