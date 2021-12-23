a892289771e0b069a01609f124f7da621d333d7b
pyliu <3634168+pyliu@users.noreply.github.com>
2021-12-23 11:38:38 +0800

    add lah-monitor-board component

---
+++ components/lah-monitor-board.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+b-card
+  b-card-title {{ header }}
+</template>
+
+<script>
+export default {
+  props: {
+    header: { type: String, default: '未設定 header' }
+  },
+  data: () => ({})
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
