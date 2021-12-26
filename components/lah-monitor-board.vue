c11b850226e9ec9b7e6c1a56615ef4d673c5964d
pyliu <3634168+pyliu@users.noreply.github.com>
2021-12-26 13:09:05 +0800

    building monitor board

--- components/lah-monitor-board.vue
+++ components/lah-monitor-board.vue
@@ -1,14 +1,34 @@
 <template lang="pug">
 b-card
-  b-card-title {{ header }}
+  template(#header): .d-flex.justify-content-between
+    b {{ header }}
+    lah-button(
+      icon="question",
+      action="breath",
+      variant="outline-success",
+      no-border,
+      no-icon-gutter,
+      @click="showModalById(modalId)",
+      title="說明"
+    )
+    lah-help-modal(
+      :modal-id="modalId",
+      :modal-title="`${header} 說明`"
+    ): slot(name="help")
+  slot
 </template>
 
 <script>
 export default {
   props: {
-    header: { type: String, default: '未設定 header' }
+    header: { type: String, default: '未設定監控標的' }
   },
-  data: () => ({})
+  data: () => ({
+    modalId: 'tmp-id'
+  }),
+  created () {
+    this.modalId = this.$utils.uuid()
+  }
 }
 </script>
 
--- pages/admin/mgt/index.vue
+++ pages/admin/mgt/index.vue
@@ -1,32 +1,35 @@
 <template lang="pug">
-  div(v-cloak)
-    lah-header
-      lah-transition(appear)
-        .d-flex.justify-content-between.w-100
-          .d-flex
-            .my-auto 地政監控儀錶板
-            lah-button(
-              v-b-modal.help-modal
-              icon="info"
-              variant="outline-success"
-              no-border
-              no-icon-gutter
-              title="說明"
-            )
-      lah-help-modal(:modal-id="'help-modal'" size="md")
-        h6 提供顯示個監控標的狀態之功能
-    b-card-group(columns)
-      lah-monitor-board
+div(v-cloak)
+  lah-header
+    lah-transition(appear)
+      .d-flex.justify-content-between.w-100
+        .d-flex
+          .my-auto 地政監控儀錶板
+          lah-button(
+            v-b-modal.help-modal,
+            icon="info",
+            variant="outline-success",
+            no-border,
+            no-icon-gutter,
+            title="說明"
+          )
+    lah-help-modal(:modal-id="'help-modal'", size="md")
+      h6 提供顯示個監控標的狀態之功能
+  b-card-group(columns)
+    lah-monitor-board(header="主資料庫")
+      template(#help)
+        h6 這是說明區塊
+      h6 這是顯示區塊
 </template>
 
 <script>
 export default {
   middleware: ['isAdmin'],
   head: {
-    title: '地政管理看板-桃園市地政局'
+    title: '地政監控儀錶板-桃園市地政局'
   },
   fetchOnServer: true,
-  methods: { }
+  methods: {}
 }
 </script>
 
