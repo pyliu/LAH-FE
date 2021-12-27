664038013579fee48ebe0ad9386e7552c6c89988
pyliu <3634168+pyliu@users.noreply.github.com>
2021-12-27 17:43:21 +0800

    display log

--- components/lah-monitor-board.vue
+++ components/lah-monitor-board.vue
@@ -17,9 +17,10 @@ b-card
       :modal-title="`${header} 說明`"
     ): slot(name="help")
   slot
-  ul: li(v-for="(item, idx) in headLogs"): a(href="#" @click="popupLogContent(item)")
-    lah-fa-icon(icon="clock" regular) {{ $utils.tsToAdDateStr(item.timestamp / 1000.0, true) }}
+  ul: li(v-for="(item, idx) in headLogs"): a(href="#" @click="popupLogContent(item)" title="顯示詳細記錄")
     .truncate {{ item.content }}
+    .d-flex.justify-content-end.small
+      lah-fa-icon.text-muted(icon="clock" regular) {{ displayDatetime(item.timestamp) }}
   template(#footer): .d-flex.justify-content-between.small.text-muted
     span {{ site }}
     span {{ timestamp }}
@@ -35,10 +36,10 @@ export default {
     light: 'danger',
     timestamp: '',
     logs: [
-      { timestamp: +new Date(), content: 'TEST' },
-      { timestamp: +new Date() + 10, content: 'TEST2TEST2TEST2TEST2TEST2TEST2TEST2TEST2TEST2' },
-      { timestamp: +new Date() + 20, content: 'TEST3' },
-      { timestamp: +new Date() + 30, content: 'TEST4TEST2TEST2TEST2TEST2TEST2TEST2TEST2TEST2TEST2TEST2TEST2' }
+      { timestamp: +new Date() / 1000, content: 'TEST' },
+      { timestamp: +new Date() / 1000 + 10, content: 'TEST2TEST2TEST2TEST2TEST2TEST2TEST2TEST2TEST2' },
+      { timestamp: +new Date() / 1000 + 20, content: 'TEST3' },
+      { timestamp: +new Date() / 1000 + 30, content: 'TEST4TEST2TEST2TEST2TEST2TEST2TEST2TEST2TEST2TEST2TEST2TEST2' }
     ],
     targets: {
       dbMain: '',
@@ -49,6 +50,13 @@ export default {
   computed: {
     headLogs () {
       return this.logs.filter((item, idx, arr) => idx < 3)
+    },
+    today () {
+      // e.g. 2021-12-27
+      const now = new Date()
+      return now.getFullYear() + '-' +
+        ('0' + (now.getMonth() + 1)).slice(-2) + '-' +
+        ('0' + now.getDate()).slice(-2)
     }
   },
   created () {
@@ -57,10 +65,13 @@ export default {
     // use this.site to determine which office
   },
   methods: {
+    displayDatetime (ts) {
+      const fullDt = this.$utils.tsToAdDateStr(ts, true)
+      return fullDt.replace(this.today, '')
+    },
     popupLogContent (item) {
       this.modal(item.content, {
-        title: `${this.header}`,
-        subtitle: `${this.$utils.tsToAdDateStr(item.timestamp / 1000.0)}`,
+        title: `${this.header} - ${this.displayDatetime(item.timestamp)}`,
         size: 'xl'
       })
     }
