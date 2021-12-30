71d30e310035d380cf25f18cc4f0ec6d0a893454
pyliu <3634168+pyliu@users.noreply.github.com>
2021-12-30 12:22:52 +0800

    wording

--- components/lah-sidebar.vue
+++ components/lah-sidebar.vue
@@ -126,7 +126,7 @@ b-sidebar#lah-sidebar(
         跨所伺服器監控面板
       li(v-if="authority.isAdmin"): nuxt-link(to="/admin/mgt").
         #[font-awesome-icon.fixed-width(:icon="['fas', 'diagnoses']", size="lg")]
-        地政監控儀錶板
+        {{site}} 智慧監控儀錶板
       li(v-if="authority.isAdmin"): nuxt-link(to="/admin/lxhweb").
         #[font-awesome-icon.fixed-width(:icon="['fab', 'watchman-monitoring']", size="lg")]
         同步異動監控
