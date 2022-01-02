7ce01a948632277beb5b4881401c1b87bae52ccd
pyliu <3634168+pyliu@users.noreply.github.com>
2022-01-02 17:27:22 +0800

    use updateData instead rebuild char every time data loading

--- components/lah-monitor-board-crossap.vue
+++ components/lah-monitor-board-crossap.vue
@@ -47,11 +47,38 @@ export default {
     header: '跨域AP連線狀態',
     bar: true,
     reloadTimer: null,
-    chartItems: []
+    loadItems: [],
+    chartDatasetIdx: 0,
+    initItems: [
+      { x: '桃園所', y: 0, color: { R: 254, G: 185, B: 180 } },
+      { x: '中壢所', y: 0, color: { R: 125, G: 199, B: 80 } },
+      { x: '大溪所', y: 0, color: { R: 255, G: 251, B: 185 } },
+      { x: '楊梅所', y: 0, color: { R: 0, G: 157, B: 122 } },
+      { x: '蘆竹所', y: 0, color: { R: 33, G: 137, B: 227 } },
+      { x: '八德所', y: 0, color: { R: 181, G: 92, B: 66 } },
+      { x: '平鎮所', y: 0, color: { R: 195, G: 42, B: 84 } },
+      { x: '龜山所', y: 0, color: { R: 136, G: 72, B: 152 } },
+      { x: '地政局', y: 0, color: { R: 207, G: 207, B: 207 } }
+    ],
+    crossApMap: new Map([
+      ['220.1.33.71', { name: '地政局', code: 'H0', ip: '220.1.33.71' }],
+      ['220.1.34.161', { name: '桃園所', code: 'HA', ip: '220.1.34.161' }],
+      ['220.1.35.123', { name: '中壢所', code: 'HB', ip: '220.1.35.123' }],
+      ['220.1.36.45', { name: '大溪所', code: 'HC', ip: '220.1.36.45' }],
+      ['220.1.37.246', { name: '楊梅所', code: 'HD', ip: '220.1.37.246' }],
+      ['220.1.38.30', { name: '蘆竹所', code: 'HE', ip: '220.1.38.30' }],
+      ['220.1.39.57', { name: '八德所', code: 'HF', ip: '220.1.39.57' }],
+      ['220.1.40.33', { name: '平鎮所', code: 'HG', ip: '220.1.40.33' }],
+      ['220.1.41.20', { name: '龜山所', code: 'HH', ip: '220.1.41.20' }]
+    ])
   }),
   computed: {
+    crossAPIp () {
+      const found = [...this.crossApMap].find(arr => arr[1].code === this.site)
+      return found[0]
+    },
     chartType () { return this.bar ? 'bar' : 'line' },
-    totalCount () { return this.chartItems.reduce((acc, item) => acc + item.y, 0) },
+    totalCount () { return this.loadItems.reduce((acc, item) => acc + item[1], 0) },
     light () {
       if (this.totalCount > 500) { return 'danger' }
       if (this.totalCount > 300) { return 'warning' }
@@ -60,7 +87,14 @@ export default {
   },
   created () {
     this.modalId = this.$utils.uuid()
-    this.load()
+  },
+  mounted () {
+    this.chartDatasetIdx = this.$refs.chart?.addDataset(this.initItems, '連線數', this.chartType)
+    this.$refs.chart?.build()
+    this.loadAPConnectionCount()
+  },
+  beforeDestroy () {
+    clearTimeout(this.reloadTimer)
   },
   methods: {
     popupMaximize () {
@@ -69,18 +103,28 @@ export default {
         size: 'xl'
       })
     },
-    load () {
+    loadAPConnectionCount () {
       clearTimeout(this.reloadTimer)
-      this.$refs.chart?.reset()
-      this.chartItems.length = 0
+      this.loadItems.length = 0
       this.$axios
         .post(this.$consts.API.JSON.STATS, {
           type: 'stats_latest_ap_conn',
-          ap_ip: this.webapIp,
+          ap_ip: this.crossAPIp,
           all: true
         })
         .then(({ data }) => {
           if (data.data_count && data.data_count > 0) {
+            const tmp = new Map([
+              ['地政局', 0],
+              ['桃園所', 0],
+              ['中壢所', 0],
+              ['大溪所', 0],
+              ['楊梅所', 0],
+              ['蘆竹所', 0],
+              ['八德所', 0],
+              ['平鎮所', 0],
+              ['龜山所', 0]
+            ])
             data.raw.forEach((item, idx, array) => {
               /*
                   item = {
@@ -92,23 +136,18 @@ export default {
                       name: '資訊主機'
                   }
               */
-              const text = this.$consts.ipMap.get(item.est_ip)?.name
-              if (text) {
-                const value = item.count
-                const found = this.chartItems.find((item) => {
-                  return item.x === text
-                })
-                if (found) {
-                  found.y += value
-                } else {
-                  this.chartItems.push({ x: text, y: value })
-                }
+              const text = this.crossApMap.get(item.est_ip)?.name
+              const currentValue = tmp.get(text)
+              if (currentValue !== undefined) {
+                tmp.set(text, item.count + currentValue)
               } else {
-                // this.$utils.warn('item.est_ip 不在 ipMap 內無法新增至 chartItems 裡', item)
+                // this.$utils.warn('item.est_ip 不在 ipMap 內無法新增至 loadItems 裡', item)
               }
             })
-            this.$refs.chart?.addDataset(this.chartItems, '跨域AP連線數', this.chartType)
-            this.$refs.chart?.rebuild()
+            this.loadItems = [...tmp]
+            this.loadItems.forEach((element) => {
+              this.$refs.chart.updateData({ x: element[0], y: element[1] }, this.chartDatasetIdx)
+            })
           }
         })
         .catch((err) => {
@@ -116,7 +155,7 @@ export default {
         })
         .finally(() => {
           // reload every 15s
-          this.reloadTimer = this.timeout(this.load, 15 * 1000)
+          this.reloadTimer = this.timeout(this.loadAPConnectionCount, 15 * 1000)
         })
     }
   }
