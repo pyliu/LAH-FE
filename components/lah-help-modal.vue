da0d39f94a29367eb28d2cb8d5f28f2e3e9ffe3a
pyliu <3634168+pyliu@users.noreply.github.com>
2022-01-20 17:15:03 +0800

    wip

--- components/lah-help-modal.vue
+++ components/lah-help-modal.vue
@@ -1,51 +1,60 @@
 <template lang="pug">
-  b-modal(
-    :id="modalId"
-    :hide-footer="hideFooter"
-    :centered="centered"
-    :size="size"
-    :scrollable="scrollable"
-    :no-close-on-backdrop="noCloseOnBackdrop"
-  )
-    template(#modal-title): div(v-html="modalTitle")
-    slot
-    hr
-    p.h6.my-2.align-middle.
-      滑鼠點擊
-      #[lah-fa-icon(icon="window-restore" regular variant="primary")]
-      開啟案件詳情視窗
+b-modal(
+  ref="help"
+  :id="modalId"
+  :hide-footer="hideFooter"
+  :centered="centered"
+  :size="size"
+  :scrollable="scrollable"
+  :no-close-on-backdrop="noCloseOnBackdrop"
+)
+  template(#modal-title): div(v-html="modalTitle")
+  slot
+  hr
+  p.h6.my-2.align-middle.
+    滑鼠點擊
+    #[lah-fa-icon(icon="window-restore" regular variant="primary")]
+    開啟案件詳情視窗
 
-    p.h6.my-2.align-middle.
-      滑鼠點擊
-      #[lah-fa-icon(icon="sync" variant="secondary")]
-      立即讀取最新資料
+  p.h6.my-2.align-middle.
+    滑鼠點擊
+    #[lah-fa-icon(icon="sync" variant="secondary")]
+    立即讀取最新資料
 
-    p.h6.my-2.align-middle.
-      滑鼠點擊
-      #[lah-fa-icon(icon="bars" variant="dark")]
-      顯示側邊攔選單
+  p.h6.my-2.align-middle.
+    滑鼠點擊
+    #[lah-fa-icon(icon="bars" variant="dark")]
+    顯示側邊攔選單
 
-    p.h6.my-2.align-middle.
-      滑鼠點擊
-      #[lah-fa-icon.mx-1(icon="info" variant="success")]
-      查看頁面說明
+  p.h6.my-2.align-middle.
+    滑鼠點擊
+    #[lah-fa-icon.mx-1(icon="info" variant="success")]
+    查看頁面說明
 
-    p.h6.my-2.align-middle.
-      滑鼠點擊
-      #[lah-fa-icon(icon="home" variant="dark")]
-      回首頁
+  p.h6.my-2.align-middle.
+    滑鼠點擊
+    #[lah-fa-icon(icon="home" variant="dark")]
+    回首頁
 </template>
 
 <script>
 export default {
   props: {
-    modalId: { type: String, required: true },
+    modalId: { type: String, default: 'modal-id', required: false },
     modalTitle: { type: String, default: '頁面說明' },
     size: { type: String, default: 'md' },
     scrollable: { type: Boolean, default: false },
     centered: { type: Boolean, default: false },
     hideFooter: { type: Boolean, default: true },
     noCloseOnBackdrop: { type: Boolean, default: false }
+  },
+  methods: {
+    show () {
+      this.$refs.help?.show()
+    },
+    hide () {
+      this.$refs.help?.hide()
+    }
   }
 }
 </script>
