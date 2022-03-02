c064e45ee520a191cc3c21388ea30aea599a79fb
pyliu <3634168+pyliu@users.noreply.github.com>
2022-03-02 08:31:38 +0800

    remove LAH-WSS

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -24,24 +24,6 @@ module.exports = {
     wait_ready: true,
     restart_delay: 5000
   }, {
-    name: 'LAH-WSS',
-    exec_mode: 'cluster',
-    instances: 1,
-    script: './ws/server.js',
-    args: 'ws',
-    out_file: '../wss_out.log',
-    error_file: '../wss_err.log',
-    cron_restart: '0 7 * * *',
-    time: true,
-    watch: true,
-    ignore_watch: ['[/\\]./', 'node_modules', '*.bat', '.git', './ws/db/*', './assets/db/*'],
-    max_memory_restart: '256M',
-    env: {
-      NODE_ENV: 'production'
-    },
-    wait_ready: true,
-    restart_delay: 5000
-  }, {
     name: 'LAH-SCHEDULER',
     exec_mode: 'cluster',
     instances: 1,
