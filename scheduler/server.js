e5008d03b22afcd277905023f970f2996d0821ec
pyliu <3634168+pyliu@users.noreply.github.com>
2022-04-19 17:44:11 +0800

    add randomDelay for the calling

--- scheduler/server.js
+++ scheduler/server.js
@@ -4,7 +4,9 @@ const qs = require('qs')
 const axios = require('axios')
 // to PHP API
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
-
+function randomDelay (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+};
 try {
   const isDev = process.env.NODE_ENV !== 'production'
   require('dotenv').config()
@@ -13,33 +15,37 @@ try {
   const watchdogCronConfig = '0 */15 7-17 * * 0-6'
   console.log(`啟動 watchdog 排程 ${watchdogCronConfig}`)
   schedule.scheduleJob(watchdogCronConfig, function () {
-    const url = `http://${process.env.API_HOST}:${process.env.API_PORT}/api/query_json_api.php`
-    axios.post(
-      url,
-      qs.stringify({ type: 'watchdog' })
-    ).then(({ data }) => {
-      console.log(data.message)
-    }).catch((err) => {
-      console.error(err)
-    }).finally(() => {
-      console.log('已完成 watchdog 請求')
-    })
+    setTimeout(() => {
+      const url = `http://${process.env.API_HOST}:${process.env.API_PORT}/api/query_json_api.php`
+      axios.post(
+        url,
+        qs.stringify({ type: 'watchdog' })
+      ).then(({ data }) => {
+        console.log(data.message)
+      }).catch((err) => {
+        console.error(err)
+      }).finally(() => {
+        console.log('已完成 watchdog 請求')
+      })
+    }, randomDelay(50, 500))
   })
 
   const schedulerCronConfig = '0 */5 7-22 * * 0-6'
   console.log(`啟動 scheduler 排程 ${schedulerCronConfig}`)
   schedule.scheduleJob(schedulerCronConfig, function () {
-    const url = `http://${process.env.API_HOST}:${process.env.API_PORT}/api/schedule_json_api.php`
-    axios.post(
-      url,
-      qs.stringify({ type: 'reqular' })
-    ).then(({ data }) => {
-      console.log(data.message)
-    }).catch((err) => {
-      console.error(err)
-    }).finally(() => {
-      console.log('已完成 scheduler 請求')
-    })
+    setTimeout(() => {
+      const url = `http://${process.env.API_HOST}:${process.env.API_PORT}/api/schedule_json_api.php`
+      axios.post(
+        url,
+        qs.stringify({ type: 'reqular' })
+      ).then(({ data }) => {
+        console.log(data.message)
+      }).catch((err) => {
+        console.error(err)
+      }).finally(() => {
+        console.log('已完成 scheduler 請求')
+      })
+    }, randomDelay(50, 500))
   })
 
   console.log('LAH排程伺服器已啟動')
