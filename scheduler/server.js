11674cc723b872904d715ea9b2f94a20df46fddc
pyliu <3634168+pyliu@users.noreply.github.com>
2022-04-19 22:46:33 +0800

    add baseAPIUrl var

--- scheduler/server.js
+++ scheduler/server.js
@@ -12,11 +12,12 @@ try {
   const isDev = process.env.NODE_ENV !== 'production'
   require('dotenv').config()
   isDev && console.log(process.env)
-
+  const baseAPIUrl = `http://${process.env.API_HOST}:${process.env.API_PORT}`
   const watchdogCronConfig = '0 */15 7-17 * * 0-6'
   console.log(`啟動 watchdog 排程 ${watchdogCronConfig}`)
   schedule.scheduleJob(watchdogCronConfig, debounce(() => {
-    const url = `http://${process.env.API_HOST}:${process.env.API_PORT}/api/query_json_api.php`
+    const url = `${baseAPIUrl}/api/query_json_api.php`
+    console.log('執行 watchdog 請求')
     axios.post(
       url,
       qs.stringify({ type: 'watchdog' })
@@ -25,14 +26,15 @@ try {
     }).catch((err) => {
       console.error(err)
     }).finally(() => {
-      console.log('已完成 watchdog 請求')
+      console.log('完成 watchdog 請求')
     })
   }, randomDelay(50, 500)))
 
   const schedulerCronConfig = '0 */5 7-22 * * 0-6'
   console.log(`啟動 scheduler 排程 ${schedulerCronConfig}`)
   schedule.scheduleJob(schedulerCronConfig, debounce(() => {
-    const url = `http://${process.env.API_HOST}:${process.env.API_PORT}/api/schedule_json_api.php`
+    const url = `${baseAPIUrl}/api/schedule_json_api.php`
+    console.log('執行 scheduler 請求')
     axios.post(
       url,
       qs.stringify({ type: 'reqular' })
@@ -41,7 +43,7 @@ try {
     }).catch((err) => {
       console.error(err)
     }).finally(() => {
-      console.log('已完成 scheduler 請求')
+      console.log('完成 scheduler 請求')
     })
   }, randomDelay(50, 500)))
 
