69f184648a8f17d557be736ff7bb04e5371ef680
pyliu <3634168+pyliu@users.noreply.github.com>
2022-04-19 17:34:26 +0800

    remove useless lock

--- scheduler/server.js
+++ scheduler/server.js
@@ -11,45 +11,35 @@ try {
   isDev && console.log(process.env)
 
   const watchdogCronConfig = '0 */15 7-17 * * 0-6'
-  let watchdogLock = false
   console.log(`啟動 watchdog 排程 ${watchdogCronConfig}`)
   schedule.scheduleJob(watchdogCronConfig, function () {
-    if (!watchdogLock) {
-      watchdogLock = true
-      const url = `http://${process.env.API_HOST}:${process.env.API_PORT}/api/query_json_api.php`
-      axios.post(
-        url,
-        qs.stringify({ type: 'watchdog' })
-      ).then(({ data }) => {
-        console.log(data.message)
-      }).catch((err) => {
-        console.error(err)
-      }).finally(() => {
-        console.log('已完成 watchdog 請求')
-        watchdogLock = false
-      })
-    }
+    const url = `http://${process.env.API_HOST}:${process.env.API_PORT}/api/query_json_api.php`
+    axios.post(
+      url,
+      qs.stringify({ type: 'watchdog' })
+    ).then(({ data }) => {
+      console.log(data.message)
+    }).catch((err) => {
+      console.error(err)
+    }).finally(() => {
+      console.log('已完成 watchdog 請求')
+    })
   })
 
   const schedulerCronConfig = '0 */5 7-22 * * 0-6'
-  let schedulerLock = false
   console.log(`啟動 scheduler 排程 ${schedulerCronConfig}`)
   schedule.scheduleJob(schedulerCronConfig, function () {
-    if (!schedulerLock) {
-      schedulerLock = true
-      const url = `http://${process.env.API_HOST}:${process.env.API_PORT}/api/schedule_json_api.php`
-      axios.post(
-        url,
-        qs.stringify({ type: 'reqular' })
-      ).then(({ data }) => {
-        console.log(data.message)
-      }).catch((err) => {
-        console.error(err)
-      }).finally(() => {
-        console.log('已完成 scheduler 請求')
-        schedulerLock = false
-      })
-    }
+    const url = `http://${process.env.API_HOST}:${process.env.API_PORT}/api/schedule_json_api.php`
+    axios.post(
+      url,
+      qs.stringify({ type: 'reqular' })
+    ).then(({ data }) => {
+      console.log(data.message)
+    }).catch((err) => {
+      console.error(err)
+    }).finally(() => {
+      console.log('已完成 scheduler 請求')
+    })
   })
 
   console.log('LAH排程伺服器已啟動')
