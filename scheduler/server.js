78b7a7f33819e1e0ead05c4a35ae872ef6a8dc73
pyliu <3634168+pyliu@users.noreply.github.com>
2022-04-19 22:38:38 +0800

    use lodash/debounce for preventing race condition

--- scheduler/server.js
+++ scheduler/server.js
@@ -1,5 +1,6 @@
 // https://www.npmjs.com/package/node-schedule
 const schedule = require('node-schedule')
+const debounce = require('lodash/debounce')
 const qs = require('qs')
 const axios = require('axios')
 // to PHP API
@@ -14,39 +15,35 @@ try {
 
   const watchdogCronConfig = '0 */15 7-17 * * 0-6'
   console.log(`啟動 watchdog 排程 ${watchdogCronConfig}`)
-  schedule.scheduleJob(watchdogCronConfig, function () {
-    setTimeout(() => {
-      const url = `http://${process.env.API_HOST}:${process.env.API_PORT}/api/query_json_api.php`
-      axios.post(
-        url,
-        qs.stringify({ type: 'watchdog' })
-      ).then(({ data }) => {
-        console.log(data.message)
-      }).catch((err) => {
-        console.error(err)
-      }).finally(() => {
-        console.log('已完成 watchdog 請求')
-      })
-    }, randomDelay(50, 500))
-  })
+  schedule.scheduleJob(watchdogCronConfig, debounce(() => {
+    const url = `http://${process.env.API_HOST}:${process.env.API_PORT}/api/query_json_api.php`
+    axios.post(
+      url,
+      qs.stringify({ type: 'watchdog' })
+    ).then(({ data }) => {
+      console.log(data.message)
+    }).catch((err) => {
+      console.error(err)
+    }).finally(() => {
+      console.log('已完成 watchdog 請求')
+    })
+  }, randomDelay(50, 500)))
 
   const schedulerCronConfig = '0 */5 7-22 * * 0-6'
   console.log(`啟動 scheduler 排程 ${schedulerCronConfig}`)
-  schedule.scheduleJob(schedulerCronConfig, function () {
-    setTimeout(() => {
-      const url = `http://${process.env.API_HOST}:${process.env.API_PORT}/api/schedule_json_api.php`
-      axios.post(
-        url,
-        qs.stringify({ type: 'reqular' })
-      ).then(({ data }) => {
-        console.log(data.message)
-      }).catch((err) => {
-        console.error(err)
-      }).finally(() => {
-        console.log('已完成 scheduler 請求')
-      })
-    }, randomDelay(50, 500))
-  })
+  schedule.scheduleJob(schedulerCronConfig, debounce(() => {
+    const url = `http://${process.env.API_HOST}:${process.env.API_PORT}/api/schedule_json_api.php`
+    axios.post(
+      url,
+      qs.stringify({ type: 'reqular' })
+    ).then(({ data }) => {
+      console.log(data.message)
+    }).catch((err) => {
+      console.error(err)
+    }).finally(() => {
+      console.log('已完成 scheduler 請求')
+    })
+  }, randomDelay(50, 500)))
 
   console.log('LAH排程伺服器已啟動')
 } catch (e) {
