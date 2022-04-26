0fa270cddaf13398433ca7180fd73f4bd1abaa69
pyliu <3634168+pyliu@users.noreply.github.com>
2022-04-26 18:40:35 +0800

    refactoring lah-footer

--- components/lah-footer.vue
+++ components/lah-footer.vue
@@ -1,15 +1,15 @@
 <template lang="pug">
-  lah-transition(slide-up appear): .p-2(v-show="show" :class="classes"): span
-    a(
-      href="https://github.com/pyliu/LAH-NUXTJS"
-      target="_blank"
-      title="View project on Github!"
-    ): i.fab.fa-github.fa-lg.text-dark.mr-1 
-    strong.
-      #[i.far.fa-copyright]
-      #[a(href="mailto:pangyu.liu@gmail.com") LIU, PANG-YU]
-      ALL RIGHTS RESERVED.
-    a(href="https://vuejs.org/" target="_blank" title="Learn Vue JS!"): i.text-success.fab.fa-vuejs.fa-lg
+lah-transition(slide-up appear): .d-flex.align-items-center.justify-content-center(v-if="show" :class="classes"): span
+  a(
+    href="https://github.com/pyliu/LAH-NUXTJS"
+    target="_blank"
+    title="View project on Github!"
+  ): i.fab.fa-github.fa-lg.text-dark.mr-1
+  strong.
+    #[i.far.fa-copyright]
+    #[a(href="mailto:pangyu.liu@gmail.com") LIU, PANG-YU]
+    ALL RIGHTS RESERVED.
+  a(href="https://vuejs.org/" target="_blank" title="Learn Vue JS!"): i.text-success.fab.fa-vuejs.fa-lg
 </template>
 
 <script>
@@ -18,27 +18,28 @@ export default {
     show: false,
     leave_time: 10000,
     classes: [
-      "text-muted",
-      "fixed-bottom",
-      "my-2",
-      "mx-3",
-      "bg-white",
-      "border",
-      "rounded",
-      "text-center",
-      "p-2",
-      "small",
-      "shadow-xl",
-    ],
+      'text-muted',
+      'fixed-bottom',
+      'my-2',
+      'mx-auto',
+      'bg-white',
+      'border',
+      'rounded',
+      'text-center',
+      'p-2',
+      'small',
+      'shadow-xl',
+      'w-50'
+    ]
   }),
-  mounted() {
+  mounted () {
     this.show = true
     this.timeout(() => (this.show = false), this.leave_time).then((handle) => {
       // save the handle here if you want to control timeout func
-    }).catch((err) => this.$utils.error(err))
+    }).catch(err => this.$utils.error(err))
     this.systemConfigs.mock && this.notify('目前系統處於模擬模式，僅會回應快取的資料', { type: 'dark', pos: 'br', delay: 7500 })
-  },
-};
+  }
+}
 </script>
 
 <style>
