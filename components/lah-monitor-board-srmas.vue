d3431102ed0d70ba4eeb52bd0eb78f3fcd1888a8
pyliu <3634168+pyliu@users.noreply.github.com>
2022-05-11 15:14:35 +0800

    wording

--- components/lah-monitor-board-srmas.vue
+++ components/lah-monitor-board-srmas.vue
@@ -74,7 +74,7 @@ b-card(:border-variant="border")
       div 🔴 表示有「告警通知」但無「回復通知」之項目
   slot
   .center(v-if="headMessages.length === 0") ⚠  {{ fetchDay }}日內無資料
-  .center.mt-5(v-else-if="problems.length === 0 && fixed.length === 0") 👌 {{ monitorHrs }}小時內沒有發生告警
+  .center.mt-5.h3(v-else-if="problems.length === 0 && fixed.length === 0") 🟢 {{ monitorHrs }}小時內一切正常
   div(v-else)
     lah-monitor-board-srmas-item.mb-2(
       v-if="problems.length > 0"
