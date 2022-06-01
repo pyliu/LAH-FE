45827b8f78bb56d4706a8c510d17d61d9a6ac3f0
pyliu <3634168+pyliu@users.noreply.github.com>
2022-06-01 13:50:47 +0800

    regex updated

--- components/lah-monitor-board-hacmp.vue
+++ components/lah-monitor-board-hacmp.vue
@@ -85,7 +85,7 @@ export default {
     fetchType: 'subject',
     fetchKeyword: 'hacmp',
     fetchDay: 1,
-    foundFsRegex: /\/.+\s+datavg\s+.+\s+ORAH[A-H]HA1,ORAH[A-H]HA2/gm
+    foundFsRegex: /\/.+\s+datavg\s+\w+\s+ORAH[A-H]HA1,ORAH[A-H]HA2/igm
   }),
   computed: {
     headMessage () {
