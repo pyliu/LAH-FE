7cdddf6e77e270d5c1fec46245ff56224472ccbb
pyliu <3634168+pyliu@users.noreply.github.com>
2022-07-18 11:59:57 +0800

    remove log

--- pages/expire/index.vue
+++ pages/expire/index.vue
@@ -298,7 +298,6 @@ export default {
             const number = alphabet.charCodeAt(0) - 64
             pipelineItems = pipelineItems.filter((item) => {
               const extractedWord = item.收件字號.match(/\(.+\)/gm)[0].replace(/[()]/gm, '')
-              this.$utils.warn(item.收件字號, extractedWord)
               if (extractedWord.startsWith(this.site)) {
                 return true
               } else if (extractedWord.endsWith(`${alphabet}1`)) {
