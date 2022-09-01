636edaf7af23a0832a0faa91a6eab4411e8470e6
pyliu <3634168+pyliu@users.noreply.github.com>
2022-09-01 15:59:13 +0800

    add bureau folder

---
+++ pages/bureau/index.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+div(v-cloak)
+  lah-header
+    lah-transition(appear)
+      .d-flex.justify-content-between.w-100.my-auto
+        .d-flex
+          div 局端監控儀錶板
+          lah-button(icon="question" variant="outline-success" no-border no-icon-gutter v-b-modal.help-modal title="說明")
+        div 右側選單區域
+    lah-help-modal(:modal-id="'help-modal'" size="xl"): lah-button(icon="exclamation-circle" variant="danger")
+  b-container.center(fluid v-cloak)
+    | some contents goes here ...
+</template>
+
+<script>
+export default {
+  head: {
+    title: '局端監控儀錶板-桃園市地政局'
+  },
+  fetchOnServer: false
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
