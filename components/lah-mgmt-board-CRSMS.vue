4ae1bd8e31094aa93e8920a9ca7be54b2c6a178d
pyliu <3634168+pyliu@users.noreply.github.com>
2022-09-08 17:05:55 +0800

    add detail modal for showing case detail

--- components/lah-mgmt-board-CRSMS.vue
+++ components/lah-mgmt-board-CRSMS.vue
@@ -1,23 +1,48 @@
 <template lang="pug">
 b-card
   template(#header)
-    h6.mb-0 登記案件快速修正
+    h6.mb-0 登記案件快速修正 {{ caseId }}
+
   b-card-body
     lah-reg-case-input-group(v-model="caseId")
+
   template(#footer)
     .d-flex.justify-content-between
-      a(href="#") 收件資料
-      a(href="#") 辦理情形
+      lah-button(icon="scroll", @click="showCaseDetail") 案件資料
+
+  b-modal(
+    ref="detail",
+    hide-footer,
+    centered,
+    no-close-on-backdrop,
+    size="xl",
+    scrollable
+  )
+    template(#modal-title) 登記案件詳情 {{ $utils.caseId(caseId) }}
+    h4.text-center.text-info.my-5(v-if="modalLoading")
+      b-spinner(small type="grow")
+        strong.ld-txt 查詢中...
+    lah-reg-case-detail(
+      :case-id="caseId",
+      @ready="modalLoading = !$event.detail"
+    )
 </template>
 
 <script>
 export default {
   data: () => ({
-    caseId: ''
+    caseId: '',
+    modalLoading: false
   }),
   watch: {
     caseId (val) {
       this.$utils.warn(val)
+    }
+  },
+  methods: {
+    showCaseDetail () {
+      this.modalLoading = true
+      this.$refs.detail.show()
     }
   }
 }
