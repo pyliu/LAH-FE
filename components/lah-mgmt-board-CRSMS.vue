2d1cb77150f93cc1ddfdd54b431c11f9a09d7106
pyliu <3634168+pyliu@users.noreply.github.com>
2022-09-08 17:44:02 +0800

    use dynamic modal

--- components/lah-mgmt-board-CRSMS.vue
+++ components/lah-mgmt-board-CRSMS.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 b-card
   template(#header)
-    h6.mb-0 登記案件快速修正 {{ caseId }}
+    h6.mb-0 登記案件快速修正 {{ $utils.caseId(caseId) }}
 
   b-card-body
     lah-reg-case-input-group(v-model="caseId")
@@ -10,22 +10,22 @@ b-card
     .d-flex.justify-content-between
       lah-button(icon="scroll", @click="showCaseDetail") 案件資料
 
-  b-modal(
-    ref="detail",
-    hide-footer,
-    centered,
-    no-close-on-backdrop,
-    size="xl",
-    scrollable
-  )
-    template(#modal-title) 登記案件詳情 {{ $utils.caseId(caseId) }}
-    h4.text-center.text-info.my-5(v-if="modalLoading")
-      b-spinner(small type="grow")
-        strong.ld-txt 查詢中...
-    lah-reg-case-detail(
-      :case-id="caseId",
-      @ready="modalLoading = !$event.detail"
-    )
+  //- b-modal(
+  //-   ref="detail",
+  //-   hide-footer,
+  //-   centered,
+  //-   no-close-on-backdrop,
+  //-   size="xl",
+  //-   scrollable
+  //- )
+  //-   template(#modal-title) 登記案件詳情 {{ $utils.caseId(caseId) }}
+  //-   h4.text-center.text-info.my-5(v-if="modalLoading")
+  //-     b-spinner(small type="grow")
+  //-       strong.ld-txt 查詢中...
+  //-   lah-reg-case-detail(
+  //-     :case-id="caseId",
+  //-     @ready="modalLoading = !$event.detail"
+  //-   )
 </template>
 
 <script>
@@ -34,15 +34,22 @@ export default {
     caseId: '',
     modalLoading: false
   }),
-  watch: {
-    caseId (val) {
-      this.$utils.warn(val)
-    }
-  },
   methods: {
     showCaseDetail () {
       this.modalLoading = true
-      this.$refs.detail.show()
+      this.modal(this.$createElement('lah-reg-case-detail', {
+        props: {
+          caseId: this.caseId
+        },
+        on: {
+          ready: (e) => {
+            this.modalLoading = !e.detail
+          }
+        }
+      }), {
+        title: `登記案件詳情 ${this.$utils.caseId(this.caseId)}`,
+        size: 'lg'
+      })
     }
   }
 }
