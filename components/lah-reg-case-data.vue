88f25dd9605cd7a0af4efd869050832a84fae13f
pyliu <3634168+pyliu@users.noreply.github.com>
2022-09-11 19:25:06 +0800

    to pug

--- components/lah-reg-case-data.vue
+++ components/lah-reg-case-data.vue
@@ -1,68 +1,47 @@
-<template>
-  <b-card>
-    <b-card-title v-if="!noTitle">
-      收件資料 <b-link :href="queryDataUrl" target="_blank" title="開啟新視窗">
-        <lah-fa-icon icon="share-square" no-gutter />
-      </b-link>
-    </b-card-title>
-    <b-list-group v-if="ready" flush compact>
-      <b-list-group-item v-if="bakedData.跨所 == 'Y'">
-        <span class="bg-info text-white rounded p-1">跨所案件 ({{ bakedData.資料收件所 }} =>
-          {{ bakedData.資料管轄所 }})</span>
-      </b-list-group-item>
-      <b-list-group-item>
-        收件字號：
-        <a
-          :title="'收件資料 on ' + webapIp"
-          href="javascript:void(0)"
-          @click="$utils.openNewWindow(queryDataUrl, $event)"
-        >
-          {{ bakedData.收件字號 }}
-        </a>
-      </b-list-group-item>
-      <b-list-group-item>
-        收件時間：{{ bakedData.收件時間 }}
-      </b-list-group-item>
-      <b-list-group-item>
-        測量案件：{{ bakedData.測量案件 }}
-      </b-list-group-item>
-      <b-list-group-item>
-        限辦期限：<span v-html="bakedData.限辦期限" />
-      </b-list-group-item>
-      <b-list-group-item>
-        作業人員：
-        <b-button pill variant="outline-secondary" size="sm" @click="userinfo(bakedData.作業人員, bakedData.RM30_1)">
-          <lah-avatar :id="bakedData.RM30_1" :name="bakedData.作業人員">
-            {{ bakedData.作業人員 }}
-          </lah-avatar>
-        </b-button>
-      </b-list-group-item>
-      <b-list-group-item>
-        辦理情形：{{ bakedData.辦理情形 }}
-      </b-list-group-item>
-      <b-list-group-item>
-        登記原因：{{ bakedData.登記原因 }}
-      </b-list-group-item>
-      <b-list-group-item>
-        區域：{{ bakedData.區名稱 }}【{{ bakedData.RM10 }}】
-      </b-list-group-item>
-      <b-list-group-item>
-        段小段：{{ bakedData.段小段 }}【{{ bakedData.段代碼 }}】
-      </b-list-group-item>
-      <b-list-group-item> 地號：{{ bakedData.地號 }} </b-list-group-item>
-      <b-list-group-item> 建號：{{ bakedData.建號 }} </b-list-group-item>
-      <b-list-group-item> 件數：{{ bakedData.件數 }} </b-list-group-item>
-      <b-list-group-item>
-        登記處理註記：{{ bakedData.登記處理註記 }}
-      </b-list-group-item>
-      <b-list-group-item>
-        地價處理註記：{{ bakedData.地價處理註記 }}
-      </b-list-group-item>
-      <b-list-group-item>
-        手機號碼：{{ bakedData.手機號碼 }}
-      </b-list-group-item>
-    </b-list-group>
-  </b-card>
+<template lang="pug">
+b-card
+  b-card-title(v-if="!noTitle")
+    | 收件資料
+    b-link(:href="queryDataUrl" target="_blank" title="開啟新視窗")
+      lah-fa-icon(icon="share-square" no-gutter)
+  b-list-group(v-if="ready" flush compact)
+    b-list-group-item(v-if="bakedData.跨所 == 'Y'")
+      span.bg-info.text-white.rounded.p-1
+        | 跨所案件 ({{ bakedData.資料收件所 }} =>
+        | {{ bakedData.資料管轄所 }})
+    b-list-group-item
+      | 收件字號：
+      a(:title="'收件資料 on ' + webapIp" href="javascript:void(0)" @click="$utils.openNewWindow(queryDataUrl, $event)")
+        | {{ bakedData.收件字號 }}
+    b-list-group-item
+      | 收件時間：{{ bakedData.收件時間 }}
+    b-list-group-item
+      | 測量案件：{{ bakedData.測量案件 }}
+    b-list-group-item
+      | 限辦期限：
+      span(v-html="bakedData.限辦期限")
+    b-list-group-item
+      | 作業人員：
+      b-button(pill variant="outline-secondary" size="sm" @click="userinfo(bakedData.作業人員, bakedData.RM30_1)")
+        lah-avatar(:id="bakedData.RM30_1" :name="bakedData.作業人員")
+          | {{ bakedData.作業人員 }}
+    b-list-group-item
+      | 辦理情形：{{ bakedData.辦理情形 }}
+    b-list-group-item
+      | 登記原因：{{ bakedData.登記原因 }}
+    b-list-group-item
+      | 區域：{{ bakedData.區名稱 }}【{{ bakedData.RM10 }}】
+    b-list-group-item
+      | 段小段：{{ bakedData.段小段 }}【{{ bakedData.段代碼 }}】
+    b-list-group-item  地號：{{ bakedData.地號 }} 
+    b-list-group-item  建號：{{ bakedData.建號 }} 
+    b-list-group-item  件數：{{ bakedData.件數 }} 
+    b-list-group-item
+      | 登記處理註記：{{ bakedData.登記處理註記 }}
+    b-list-group-item
+      | 地價處理註記：{{ bakedData.地價處理註記 }}
+    b-list-group-item
+      | 手機號碼：{{ bakedData.手機號碼 }}
 </template>
 
 <script>
