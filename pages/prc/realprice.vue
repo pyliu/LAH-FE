987bb879f12bc78dc7e532764e3833375a3bb264
pyliu <3634168+pyliu@users.noreply.github.com>
2022-12-02 16:52:00 +0800

    realprice wip

---
+++ pages/prc/realprice.vue
@@ -0,0 +1,473 @@
+<template lang="pug">
+div
+  lah-header
+    lah-transition(appear): .d-flex.justify-content-between.w-100
+      .d-flex
+        .my-auto 實價登錄案件控管
+        lah-button(icon="info" action="bounce" variant="outline-success" no-border no-icon-gutter @click="showModalById('help-modal')" title="說明")
+        lah-help-modal(:modal-id="'help-modal'")
+          h5 請參照下列步驟搜尋
+          ol
+            li 選擇日期區間(預設為目前月份)
+            li 點擊 #[lah-fa-icon(icon="search" variant="primary" no-gutter)] 搜尋
+
+      .d-flex
+        b-datepicker(
+          v-model="startDateObj"
+          placeholder="開始日期"
+          boundary="viewport"
+          size="sm"
+          :date-format-options="{ weekday: 'narrow' }"
+          :max="yesterday"
+          value-as-date
+          hide-header
+          dropleft
+        )
+        .my-auto ～
+        b-datepicker.mr-1(
+          v-model="endDateObj"
+          placeholder="截止日期"
+          boundary="viewport"
+          size="sm"
+          :date-format-options="{ weekday: 'narrow' }"
+          :max="today"
+          :min="startDateObj"
+          hide-header
+          dark
+          value-as-date
+        )
+        lah-button.mx-1(
+          icon="search"
+          size="lg"
+          title="搜尋"
+          @click="$fetch"
+          :disabled="isBusy"
+          :busy="isBusy"
+          no-icon-gutter
+        )
+        lah-button(
+          icon="search-plus",
+          size="lg",
+          title="開啟進階搜尋視窗",
+          @click="$refs.searchPlus.show()",
+          :disabled="!committed || paginationCount === 0",
+          no-icon-gutter
+        )
+        lah-button-xlsx.mx-1(
+          :jsons="xlsxData",
+          header="非專業代理人案件"
+        )
+        lah-countdown-button(
+          ref="countdown"
+          icon="sync-alt"
+          action="ld-cycle"
+          size="lg"
+          variant="outline-secondary"
+          badge-variant="secondary"
+          title="強制重新搜尋"
+          :milliseconds="0"
+          :disabled="isBusy"
+          :busy="isBusy"
+          @end="reload"
+          @click="reload"
+          no-badge
+        )
+
+  lah-transition: b-tags.border-0.mt-n4(
+    v-if="advTags.length > 0",
+    v-model="advTags",
+    placeholder="",
+    tag-variant="info",
+    tag-pills,
+    no-outer-focus,
+    no-add-on-enter,
+    no-tag-remove,
+    add-button-variant="white"
+    add-button-text=""
+  )
+  .d-flex.mb-1
+    b-pagination(
+      v-if="showPagination"
+      v-model="currentPage"
+      :total-rows="paginationCount"
+      :per-page="perPage"
+      last-number
+      first-number
+      aria-controls="scrivener-table"
+      class="my-auto mr-2"
+    )
+  lah-transition
+    div(v-if="committed")
+      lah-reg-b-table(
+        :busy="isBusy"
+        :baked-data="filterRegBakedData"
+        :fields="regFields"
+        :per-page="perPage"
+        :current-page="currentPage"
+        :caption-append="captionRange"
+        :max-height-offset="160"
+      )
+    h3(v-else class="text-center"): lah-fa-icon(icon="search" action="breath" variant="primary") 請點擊查詢按鈕
+
+  b-modal(
+    ref="searchPlus",
+    title="進階搜尋",
+    hide-footer
+  )
+    .center.d-flex.my-1
+      b-input-group.mr-1(prepend="統編"): b-select(
+        v-model="advOpts.id",
+        :options="advOpts.idOpts",
+        title="非專代統編"
+      )
+      b-input-group(prepend="名稱"): b-select(
+        v-model="advOpts.name",
+        :options="advOpts.nameOpts",
+        title="非專代名稱"
+      )
+    .center.d-flex.my-1
+      b-input-group.mr-1(prepend="電話"): b-select(
+        v-model="advOpts.tel",
+        :options="advOpts.telOpts",
+        title="非專代電話"
+      )
+      b-input-group(prepend="日期"): b-select(
+        v-model="advOpts.date",
+        :options="advOpts.dateOpts",
+        title="收件日期"
+      )
+    .center.d-flex.my-1
+      lah-button(
+        icon="recycle",
+        @click="resetAdvOpts",
+        variant="outline-success"
+      ) 重設
+      small.text-muted.ml-2 找到 {{ filterDataCount }} 筆
+</template>
+
+<script>
+export default {
+  fetchOnServer: false,
+  asyncData (nuxt) {
+    const today = new Date()
+    const yesterday = new Date(new Date().setDate(new Date().getDate() - 1))
+    const firstDayofMonth = new Date(today.getFullYear(), today.getMonth(), 1)
+    const lastDayofMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0)
+    return {
+      startDateObj: firstDayofMonth,
+      endDateObj: lastDayofMonth,
+      startDate: `${firstDayofMonth.getFullYear() - 1911}${('0' + (firstDayofMonth.getMonth() + 1)).slice(-2)}${('0' + firstDayofMonth.getDate()).slice(-2)}`,
+      endDate: `${lastDayofMonth.getFullYear() - 1911}${('0' + (lastDayofMonth.getMonth() + 1)).slice(-2)}${('0' + lastDayofMonth.getDate()).slice(-2)}`,
+      firstDayofMonth,
+      lastDayofMonth,
+      today,
+      yesterday
+    }
+  },
+  data: () => ({
+    clickedId: undefined,
+    forceReload: false,
+    committed: false,
+    regBakedData: [],
+    regFields: [
+      {
+        key: '段小段',
+        sortable: true
+      },
+      {
+        key: 'RM12',
+        label: '地號',
+        sortable: true
+      },
+      {
+        key: 'RM15',
+        label: '建號',
+        sortable: true
+      },
+      {
+        key: 'P1MP_CASENO',
+        label: '申報書序號',
+        sortable: true
+      },
+      {
+        key: '收件字號',
+        sortable: true
+      }
+      // {
+      //   key: 'P1MP_CASETYPE',
+      //   label: '申報類別',
+      //   sortable: true
+      // },
+      // {
+      //   key: 'P1MP_INSDATE',
+      //   label: '輸入日期',
+      //   sortable: true
+      // },
+      // {
+      //   key: 'P1MP_INSTIME',
+      //   label: '輸入時間',
+      //   sortable: true
+      // },
+      // {
+      //   key: 'P1MP_INSUSER',
+      //   label: '輸入人員',
+      //   sortable: true
+      // }
+    ],
+    currentPage: 1,
+    perPage: 20,
+    advOpts: {
+      id: '',
+      idOpts: [],
+      name: '',
+      nameOpts: [],
+      tel: '',
+      telOpts: [],
+      date: '',
+      dateOpts: []
+    }
+  }),
+  fetch () {
+    // reset cached data
+    this.reset()
+    // restore cached data if found
+    this.getCache(this.cacheKey).then((json) => {
+      if (this.forceReload || json === false) {
+        if (this.isBusy) {
+          this.notify('讀取中 ... 請稍後再試', { type: 'warning' })
+        } else {
+          this.isBusy = true
+          this.$axios.post(this.$consts.API.JSON.PREFETCH, {
+            type: 'val_realprice_map',
+            start_date: this.startDate,
+            end_date: this.endDate,
+            reload: this.forceReload
+          }).then(({ data }) => {
+            this.regBakedData = data.baked || []
+            this.notify(data.message, { type: this.$utils.statusCheck(data.status) ? 'info' : 'warning' })
+            const remainS = data.cache_remaining_time // in seconds
+            const remainMs = remainS * 1000
+            if (remainMs && remainMs > 0) {
+              this.setCache(this.cacheKey, data, remainMs)
+              if (this.$refs.countdown) {
+                this.$refs.countdown.setCountdown(remainMs)
+                this.$refs.countdown.startCountdown()
+              }
+            }
+          }).catch((err) => {
+            this.alert(err.message)
+            this.$utils.error(err)
+          }).finally(() => {
+            this.loaded()
+          })
+        }
+      } else {
+        this.regBakedData = json.baked
+        this.loaded()
+        this.getCacheExpireRemainingTime(this.cacheKey).then((remaining) => {
+          this.notify(`查詢成功，找到 ${this.regBakedData.length} 筆實價登錄控管案件。`, { subtitle: `(快取) ${this.$utils.msToHuman(remaining)} 後更新` })
+          if (this.$refs.countdown) {
+            this.$refs.countdown.setCountdown(remaining)
+            this.$refs.countdown.startCountdown()
+          }
+        })
+      }
+    })
+  },
+  head: {
+    title: '實價登錄案件控管-桃園市地政局'
+  },
+  computed: {
+    md5Hash () {
+      return this.$utils.md5(`${this.startDate}_${this.endDate}`)
+    },
+    cacheKey () {
+      return `realprice_mgmt_${this.md5Hash}`
+    },
+    captionRange () {
+      return `【${this.startDate.substring(0, 3)}-${this.startDate.substring(3, 5)}-${this.startDate.substring(5)} ~ ${this.endDate.substring(0, 3)}-${this.endDate.substring(3, 5)}-${this.endDate.substring(5)}】`
+    },
+    showPagination () {
+      return !this.$utils.empty(this.filterRegBakedData) && this.filterRegBakedData.length > this.perPage
+    },
+    paginationCount () {
+      return this.filterRegBakedData.length
+    },
+    advTags () {
+      const tags = []
+      if (!this.$utils.empty(this.advOpts.id)) {
+        tags.push(`統編：${this.advOpts.id}`)
+      }
+      if (!this.$utils.empty(this.advOpts.name)) {
+        tags.push(`名稱：${this.advOpts.name}`)
+      }
+      if (!this.$utils.empty(this.advOpts.tel)) {
+        tags.push(`非專代電話：${this.advOpts.tel}`)
+      }
+      if (!this.$utils.empty(this.advOpts.date)) {
+        tags.push(`收件日期：${this.advOpts.date}`)
+      }
+      return tags
+    },
+    filterRegBakedData () {
+      return this.filterBakedData(this.regBakedData)
+    },
+    filterDataCount () {
+      return this.filterRegBakedData.length
+    },
+    xlsxData () {
+      return this.prepareRegJsons()
+    }
+  },
+  watch: {
+    startDateObj (val) {
+      this.startDate = `${val.getFullYear() - 1911}${('0' + (val.getMonth() + 1)).slice(-2)}${('0' + val.getDate()).slice(-2)}`
+    },
+    endDateObj (val) {
+      this.endDate = `${val.getFullYear() - 1911}${('0' + (val.getMonth() + 1)).slice(-2)}${('0' + val.getDate()).slice(-2)}`
+    },
+    regBakedData (val) {
+      this.refreshAdvOptsSelect(val)
+    },
+    filterRegBakedData (dontcare) {
+      this.currentPage = 1
+    }
+  },
+  methods: {
+    reload () {
+      this.forceReload = true
+      this.$fetch()
+    },
+    reset () {
+      this.committed = false
+      this.regBakedData = []
+      this.currentPage = 1
+    },
+    loaded () {
+      this.isBusy = false
+      this.forceReload = false
+      this.committed = true
+    },
+    prepareRegJsons () {
+      const fieldKeys = this.regFields.map((field, idx, array) => field.key)
+      return this.filterRegBakedData.map((data, idx, array) => {
+        const obj = {}
+        for (const [key, value] of Object.entries(data)) {
+          if (fieldKeys.includes(key)) {
+            switch (key) {
+              case 'AB01':
+                obj['非專代統編'] = value
+                break
+              case 'AB02':
+                obj['非專代名稱'] = value
+                break
+              case 'AB03':
+                obj['非專代住址'] = value
+                break
+              case 'AB04_NON_SCRIVENER_TEL':
+                obj['非專代電話'] = value
+                break
+              case 'AB13':
+                obj['當年案件量'] = value
+                break
+              case 'AB23':
+                obj['本所案件量'] = value
+                break
+              case 'RM12_C':
+                obj['地號'] = value
+                break
+              case 'RM15_C':
+                obj['建號'] = value
+                break
+              default:
+                obj[key] = value
+            }
+          }
+        }
+        return obj
+      })
+    },
+    resetAdvOpts () {
+      this.advOpts = {
+        ...this.advOpts,
+        ...{
+          id: '',
+          name: '',
+          tel: '',
+          date: ''
+        }
+      }
+    },
+    refreshAdvOptsSelect (val) {
+      this.advOpts = {
+        ...{
+          id: '',
+          idOpts: [],
+          name: '',
+          nameOpts: [],
+          tel: '',
+          telOpts: [],
+          date: '',
+          dateOpts: []
+        }
+      }
+      if (val) {
+        this.advOpts.idOpts = [...new Set(val.map(item => item.AB01))].sort()
+        this.advOpts.nameOpts = [...new Set(val.map(item => item.AB02))].sort()
+        this.advOpts.telOpts = [...new Set(val.map(item => item.AB04_NON_SCRIVENER_TEL))].sort()
+        this.advOpts.dateOpts = [...new Set(val.map(item => item.收件日期))].sort()
+        this.advOpts.idOpts.unshift('')
+        this.advOpts.nameOpts.unshift('')
+        this.advOpts.telOpts.unshift('')
+        this.advOpts.dateOpts.unshift('')
+      }
+    },
+    filterBakedData (source) {
+      if (this.advTags.length > 0) {
+        let pipelineItems = source
+        const checkId = !this.$utils.empty(this.advOpts.id)
+        if (checkId) {
+          pipelineItems = pipelineItems.filter((item) => {
+            return item.AB01.match(this.advOpts.id) !== null
+          })
+        }
+        const checkName = !this.$utils.empty(this.advOpts.name)
+        if (checkName) {
+          pipelineItems = pipelineItems.filter((item) => {
+            return item.AB02 === this.advOpts.name
+          })
+        }
+        const checkTel = !this.$utils.empty(this.advOpts.tel)
+        if (checkTel) {
+          pipelineItems = pipelineItems.filter((item) => {
+            if (item.AB04_NON_SCRIVENER_TEL) {
+              return item.AB04_NON_SCRIVENER_TEL.match(this.advOpts.tel) !== null
+            }
+            return false
+          })
+        }
+        const checkDate = !this.$utils.empty(this.advOpts.date)
+        if (checkDate) {
+          pipelineItems = pipelineItems.filter((item) => {
+            return item.收件日期.match(this.advOpts.date) !== null
+          })
+        }
+        return pipelineItems
+      }
+      return source
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.move-table-up {
+  margin-top: -25px;
+}
+.fixed-height-table {
+  max-height: calc(100% - 135px);
+}
+.tags-input {
+  width: 100vw;
+}
+</style>
