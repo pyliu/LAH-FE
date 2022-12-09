3e135609c73c48536f3f5bef3497234f002567be
pyliu <3634168+pyliu@users.noreply.github.com>
2022-12-09 11:02:27 +0800

    export xlsx impl

--- pages/prc/json-converter.vue
+++ pages/prc/json-converter.vue
@@ -27,7 +27,7 @@ div
         b-badge(variant="dark") {{ names[0] }}
         b-badge(v-if="names.length > 1" variant="dark" class="ml-1").
           + {{ names.length - 1 }} 更多檔案
-    lah-button(
+    lah-button.mr-1(
         v-if="file !== null"
         icon="trash",
         title="清除選擇",
@@ -35,12 +35,13 @@ div
         @click="file = null",
         no-icon-gutter
       )
+    .text-danger.my-auto.h5(v-if="showError") ⚠️ 不是支援的JSON格式檔案(資料需有 "ZWw1MDEw" 欄位)。
   lah-val-realprice-json-list(
-    v-if="Array.isArray(jsons) && jsons.length > 0",
+    v-if="validJsons",
     :jsons="jsons"
   )
   lah-val-realprice-json-data(
-    v-if="jsonData",
+    v-if="validJson",
     :json-data="jsonData"
   )
 </template>
@@ -50,18 +51,288 @@ export default {
   data: () => ({
     file: null,
     jsons: [],
-    jsonData: null
+    jsonData: null,
+    mainKey: 'ZWw1MDEw', // the key in the raw data
+    fields: [
+      {
+        key: 'case_no',
+        label: '申報序號',
+        sortable: true
+      },
+      {
+        key: 'apply_no',
+        label: '備查序號',
+        sortable: true
+      },
+      {
+        key: 'case_kind',
+        label: '案件種類',
+        sortable: true
+      },
+      {
+        key: 'apply_name',
+        label: '不動產經紀業名稱',
+        sortable: true
+      },
+      {
+        key: 'apply_idNo',
+        label: '不動產經紀業ID',
+        sortable: true
+      },
+      {
+        key: 'apply_tel',
+        label: '不動產經紀業電話',
+        sortable: true
+      },
+      {
+        key: 'agents_name',
+        label: '不動產經紀業代理人',
+        sortable: true
+      },
+      {
+        key: 'agents_idNo',
+        label: '不動產經紀業代理人ID',
+        sortable: true
+      },
+      {
+        key: 'agents_tel',
+        label: '不動產經紀業代理人電話',
+        sortable: true
+      },
+      {
+        key: 'right_name',
+        label: '買受人',
+        sortable: true
+      },
+      {
+        key: 'right_idNo',
+        label: '買受人ID',
+        sortable: true
+      },
+      {
+        key: 'p1ma_dd09',
+        label: '交易標的興建位置',
+        sortable: true
+      },
+      {
+        key: 'p1ma_typeB_1',
+        label: '建案名稱',
+        sortable: true
+      },
+      {
+        key: 'p1ma_typeB_2',
+        label: '起造人',
+        sortable: true
+      },
+      {
+        key: 'p1ma_typeB_3',
+        label: '建照字號',
+        sortable: true
+      },
+      {
+        key: 'p1ma_typeB_4',
+        label: '建照核發日',
+        sortable: true
+      },
+      {
+        key: 'p1ma_typeB_5',
+        label: '棟別',
+        sortable: true
+      },
+      {
+        key: 'p1ma_typeB_6',
+        label: '號',
+        sortable: true
+      },
+      {
+        key: 'p1ma_build10_1',
+        label: '交易樓層',
+        sortable: true
+      },
+      {
+        key: 'p1ma_build9',
+        label: '總樓層',
+        sortable: true
+      },
+      {
+        key: 'p1ma_build7c',
+        label: '使用別',
+        sortable: true
+      },
+      {
+        key: 'p1ma_build5c',
+        label: '建物別',
+        sortable: true
+      },
+      {
+        key: 'p1ma_build8c',
+        label: '建築別',
+        sortable: true
+      },
+      {
+        key: 'p1ma_build1',
+        label: '房間',
+        sortable: true
+      },
+      {
+        key: 'p1ma_build2',
+        label: '廳室',
+        sortable: true
+      },
+      {
+        key: 'p1ma_build3',
+        label: '衛浴',
+        sortable: true
+      },
+      {
+        key: 'p1ma_build4',
+        label: '隔間',
+        sortable: true
+      },
+      {
+        key: 'p1ma_date',
+        label: '交易日期',
+        sortable: true
+      },
+      {
+        key: 'p1ma_cntalid',
+        label: '土地筆數',
+        sortable: true
+      },
+      {
+        key: 'p1ma_cntdbid',
+        label: '建物棟數',
+        sortable: true
+      },
+      {
+        key: 'p1ma_cntpark',
+        label: '車位個數',
+        sortable: true
+      },
+      {
+        key: 'p1ma_topprice',
+        label: '總價',
+        sortable: true
+      },
+      {
+        key: 'p1ma_alidprice',
+        label: '土地價格',
+        sortable: true
+      },
+      {
+        key: 'p1ma_dbidprice',
+        label: '建物價格',
+        sortable: true
+      },
+      {
+        key: 'p1ma_parkprice',
+        label: '車位價格',
+        sortable: true
+      },
+      {
+        key: 'p1ma_parkflad',
+        label: '車位單獨計價',
+        sortable: true
+      },
+      {
+        key: 'p1ma_note',
+        label: '其他備註',
+        sortable: true
+      },
+      {
+        key: 'p1sp_price0101',
+        label: '裝潢費',
+        sortable: true
+      },
+      {
+        key: 'p1sp_price0102',
+        label: '家俱費',
+        sortable: true
+      },
+      {
+        key: 'p1sp_price0106',
+        label: '其他費',
+        sortable: true
+      },
+      {
+        key: 'p1sp_code0201',
+        label: '親友交易',
+        sortable: true
+      },
+      {
+        key: 'p1sp_code0202',
+        label: '合建',
+        sortable: true
+      },
+      {
+        key: 'p1sp_code0501',
+        label: '急買急賣',
+        sortable: true
+      },
+      {
+        key: 'p1sp_code0502',
+        label: '民情風俗',
+        sortable: true
+      },
+      {
+        key: 'p1sp_code0512',
+        label: '期待因素',
+        sortable: true
+      },
+      {
+        key: 'p1sp_code0509',
+        label: '債務',
+        sortable: true
+      },
+      {
+        key: 'p1sp_code0602',
+        label: '地上權',
+        sortable: true
+      },
+      {
+        key: 'p1sp_code0505',
+        label: '毛胚屋',
+        sortable: true
+      },
+      {
+        key: 'p1sp_desc1301',
+        label: '解約原序號',
+        sortable: true
+      },
+      {
+        key: 'p1sp_desc1401',
+        label: '其他買受人',
+        sortable: true
+      }
+    ]
   }),
-  fetch () {},
   head: {
     title: '實價登錄案件資料轉換-桃園市地政局'
   },
   computed: {
-    md5Hash () {
-      return this.$utils.md5(`${this.startDate}_${this.endDate}`)
+    validJsons () {
+      return Array.isArray(this.jsons) && this.jsons.length > 0 && this.jsons[0][this.mainKey]
     },
-    cacheKey () {
-      return `realprice_json_convert_${this.md5Hash}`
+    validJson () {
+      return this.jsonData && this.jsonData[this.mainKey]
+    },
+    showError () {
+      if (this.file) {
+        if (this.jsons && this.jsons?.length > 0 && !this.validJsons) {
+          return true
+        }
+        if (this.jsonData && !this.validJson) {
+          return true
+        }
+      }
+      return false
+    },
+    keyLabelMap () {
+      const keyLabelMap = new Map()
+      this.fields.forEach((field, idx, array) => {
+        keyLabelMap.set(field.key, field.label || field.key)
+      })
+      return keyLabelMap
     },
     xlsxData () {
       return this.prepareFormattedJson()
@@ -87,9 +358,6 @@ export default {
         this.alert(e.message)
         this.$utils.error(e)
       }
-    },
-    jsonData (val) {
-      console.warn(val)
     }
   },
   mounted () {
@@ -102,14 +370,26 @@ export default {
       this.jsonData = null
       this.jsons = []
     },
-    clearCache () {
-      this.removeCache(this.cacheKey)
-    },
     loaded () {
       this.isBusy = false
     },
+    prepareTranslatedObj (json) {
+      const data = json[this.mainKey]
+      const obj = {}
+      if (data) {
+        for (const [key, value] of Object.entries(data)) {
+          const label = this.keyLabelMap.get(key)
+          label && (obj[label] = value)
+        }
+      }
+      return obj
+    },
     prepareFormattedJson () {
-      return []
+      if (this.jsonData) {
+        return [this.prepareTranslatedObj(this.jsonData)]
+      } else {
+        return this.jsons.map(raw => this.prepareTranslatedObj(raw))
+      }
     }
   }
 }
@@ -117,6 +397,6 @@ export default {
 
 <style lang="scss" scoped>
 .fixed-width {
-  max-width: 25vw;
+  max-width: 35vw;
 }
 </style>
