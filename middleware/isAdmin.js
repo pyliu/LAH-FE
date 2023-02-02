615029b6225f0ac93c12335bef3ebd0fce4d766d
pyliu <3634168+pyliu@users.noreply.github.com>
2023-02-02 17:57:37 +0800

    add authority check back

--- middleware/isAdmin.js
+++ middleware/isAdmin.js
@@ -2,14 +2,10 @@ export default function ({
   store,
   redirect
 }) {
-  // const userinfo = store.getters.user
-  // not inf unit => redirect to '/'
-  // if (!userinfo.unit !== '資訊課') {
-  //   return redirect('/')
-  // }
+  const userinfo = store.getters.user
   const authority = store.getters.authority
   // not admin/super => redirect to '/login'
-  if (!authority.isAdmin) {
+  if (!authority.isAdmin && !userinfo.unit !== '資訊課') {
     return redirect('/login')
   }
 }
