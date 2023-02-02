856df5aae32c03e91c55ca154c70ab741a499184
pyliu <3634168+pyliu@users.noreply.github.com>
2023-02-02 17:04:03 +0800

    use old logic

--- middleware/isAdmin.js
+++ middleware/isAdmin.js
@@ -2,9 +2,14 @@ export default function ({
   store,
   redirect
 }) {
-  const userinfo = store.getters.user
+  // const userinfo = store.getters.user
   // not inf unit => redirect to '/'
-  if (!userinfo.unit !== '資訊課') {
-    return redirect('/')
+  // if (!userinfo.unit !== '資訊課') {
+  //   return redirect('/')
+  // }
+  const authority = store.getters.authority
+  // not admin/super => redirect to '/login'
+  if (!authority.isAdmin) {
+    return redirect('/login')
   }
 }
