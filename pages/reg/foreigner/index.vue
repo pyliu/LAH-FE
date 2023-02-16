6b5ed411a3539ca705f26b37c0b2c3e972c40823
pyliu <3634168+pyliu@users.noreply.github.com>
2023-02-16 17:43:07 +0800

    title wording

--- pages/reg/foreigner/index.vue
+++ pages/reg/foreigner/index.vue
@@ -3,7 +3,7 @@ div
   lah-header: lah-transition(appear)
     .d-flex.justify-content-between.w-100
       .d-flex
-        .my-auto 外國人資料建置及查詢
+        .my-auto 外國人資料查詢及建置
         lah-button(icon="info" action="bounce" variant="outline-success" no-border no-icon-gutter @click="$refs.help_modal.show()" title="說明")
         lah-help-modal(ref="help_modal")
           h5 建檔說明
@@ -47,7 +47,7 @@ div
         )
         lah-button-xlsx(
           :jsons="xlsxData"
-          header="土地參考資訊檔異動情形"
+          header="外國人資料查詢"
         )
 
   lah-pagination(
@@ -171,7 +171,7 @@ export default {
     }
   },
   head: {
-    title: '外國人資料建置及查詢-桃園市地政局'
+    title: '外國人資料查詢及建置-桃園市地政局'
   },
   computed: {
     dataReady () { return this.rows.length > 0 },
