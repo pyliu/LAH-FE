aafcd598c074c6dbf88a635bf15b27db2dc277ec
pyliu <3634168+pyliu@users.noreply.github.com>
2023-03-15 13:54:59 +0800

    use siteUserNames

--- components/lah-user-select.vue
+++ components/lah-user-select.vue
@@ -15,7 +15,7 @@ div
       variant="outline-primary"
       rounded
       @click="update(id)"
-    ) {{ id }}:{{ userNames[id].replace(/^(桃園|中壢|大溪|楊梅|蘆竹|八德|平鎮|龜山|龍潭|復興)/g, '').replace(/^(所)/g, '').replace(/([1-9_\s]|離職|分機)/g, '') }}
+    ) {{ id }}:{{ siteUserNames[id].replace(/^(桃園|中壢|大溪|楊梅|蘆竹|八德|平鎮|龜山|龍潭|復興)/g, '').replace(/^(所)/g, '').replace(/([1-9_\s]|離職|分機)/g, '') }}
   h4.center.mt-1(v-else) 查無資料
 </template>
 
@@ -39,9 +39,9 @@ export default {
       }
       const idRE = new RegExp(`^${this.keyword}`, 'gi')
       const nameRE = new RegExp(`${this.keyword}`, 'gi')
-      return Object.keys(this.userNames).filter((id) => {
-        return (id.match(idRE) && !this.userNames[id].match(/^(登記|地價|測量|資訊|主任|秘書|第|服|會|地|TEST|HH|建)/gi)) ||
-               (this.userNames[id] && this.userNames[id].match(nameRE))
+      return Object.keys(this.siteUserNames).filter((id) => {
+        return (id.match(idRE) && !this.siteUserNames[id].match(/^(登記|地價|測量|資訊|主任|秘書|第|服|會|地|TEST|HH|建)/gi)) ||
+               (this.siteUserNames[id] && this.siteUserNames[id].match(nameRE))
       })
     }
   },
