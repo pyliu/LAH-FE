2dbb131a0899f843e4c0fd26a145556c4434e138
pyliu <3634168+pyliu@users.noreply.github.com>
2023-06-02 15:22:26 +0800

    add MOICAT api EP

--- plugins/global-init.js
+++ plugins/global-init.js
@@ -113,6 +113,7 @@ export default ({ $axios, store }, inject) => {
         MOIEXP: '/api/moiexp_json_api.php',
         MOIPRC: '/api/moiprc_json_api.php',
         MOICAS: '/api/moicas_json_api.php',
+        MOICAT: '/api/moicat_json_api.php',
         REG: '/api/reg_json_api.php'
       },
       FILE: {
