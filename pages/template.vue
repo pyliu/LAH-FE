99fcf773d9a67108868fe7190e7e528ed27a91aa
pyliu <3634168+pyliu@users.noreply.github.com>
2023-06-06 15:14:47 +0800

    update template page

--- pages/template.vue
+++ pages/template.vue
@@ -1,15 +1,14 @@
 <template lang="pug">
-  div(v-cloak)
-    lah-header
-      lah-transition(appear)
-        .d-flex.justify-content-between.w-100.my-auto
-          .d-flex
-            div 標題區域
-            lah-button(icon="question" variant="outline-success" no-border no-icon-gutter v-b-modal.help-modal title="說明")
-          div 右側選單區域
-      lah-help-modal(:modal-id="'help-modal'" size="xl"): lah-button(icon="exclamation-circle" variant="danger")
-    b-container.center(fluid v-cloak)
-      | some contents goes here ...
+div(v-cloak)
+  lah-header
+    lah-transition(appear)
+      .d-flex.justify-content-between.w-100.my-auto
+        .d-flex
+          div 標題區域
+          lah-button(icon="question" variant="outline-success" no-border no-icon-gutter v-b-modal.help-modal title="說明")
+        div 右側選單區域
+    lah-help-modal(:modal-id="'help-modal'" size="xl"): lah-button(icon="exclamation-circle" variant="danger")
+  //- below is the customize area
 </template>
 
 <script>
