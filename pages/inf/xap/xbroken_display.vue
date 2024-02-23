87c5c73b96c905ee9cf9d51e263bcd4eb6768927
pyliu <3634168+pyliu@users.noreply.github.com>
2024-02-23 11:36:48 +0800

    beautifier

--- pages/inf/xap/xbroken_display.vue
+++ pages/inf/xap/xbroken_display.vue
@@ -1,16 +1,20 @@
 <template lang="pug">
-div(v-cloak)
-  lah-header
-    lah-transition(appear)
-      .d-flex.justify-content-center.w-100.my-auto
-        .d-flex
-          .h1 跨域ONLINE即時通，服務無礙一點通
-          //- lah-button(icon="question" variant="outline-success" no-border no-icon-gutter v-b-modal.help-modal title="說明")
-        div
-    lah-help-modal(:modal-id="'help-modal'" size="xl"): lah-button(icon="exclamation-circle" variant="danger")
+.body(v-cloak)
+  //- lah-header
+  //-   lah-transition(appear)
+  //-     .d-flex.justify-content-center.w-100.my-auto
+  //-       .d-flex
+  //-         .h1 跨域ONLINE即時通，服務無礙一點通
+  //-         //- lah-button(icon="question" variant="outline-success" no-border no-icon-gutter v-b-modal.help-modal title="說明")
+  //-       div
+  //-   lah-help-modal(:modal-id="'help-modal'" size="xl"): lah-button(icon="exclamation-circle" variant="danger")
   //- below is the customize area
   //- b-card-group(deck)
-  lah-fa-icon.h1(icon="wave-square", action="squeeze") 桃園市各所狀態
+  .d-flex.justify-content-center.w-100.my-auto
+    .d-flex.my-5
+      .h1 跨域ONLINE即時通，服務無礙一點通
+  .my-4
+  lah-fa-icon.h2(icon="wave-square", action="squeeze") 桃園市各所狀態
   hr.my-3
   client-only: .offices.justify-content-between
     lah-badge-site-status.office(
@@ -26,11 +30,16 @@ div(v-cloak)
       short-alt
     )
   .my-3
-  lah-fa-icon.h1(
-    icon="heart-pulse",
-    action="heartbeat",
-    :variant="downOffices.length === 0 ? 'success' : 'danger'"
-  ) 無法提供服務的地所 (更新時間：#[div {{ updatedTime }}])
+  .d-flex.justify-content-between.align-items-center
+    lah-fa-icon.h2(
+      icon="heart-pulse",
+      action="heartbeat",
+      :variant="downOffices.length === 0 ? 'success' : 'danger'"
+    ) 無法提供服務的地所
+    lah-fa-icon.h3(
+      icon="clock",
+      variant="muted"
+    ) 更新時間：{{ updatedTime }}
   hr.my-3
   .h1.center(
     v-if="downOffices.length === 0"
@@ -47,7 +56,9 @@ div(v-cloak)
       :badge="false",
       short-alt
     )
-
+  .fix-logo
+  .fix-taogirl-L
+  .fix-taogirl-R
 </template>
 
 <script>
@@ -123,5 +134,35 @@ export default {
   border: 1px solid gray;
   border-radius: 15px;
   font-size: xx-large;
+}
+.fix-logo {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 329px;
+  height: 97px;
+  background-image: url(~/assets/img/taoyuan_logo.png);
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.fix-taogirl-R {
+  position: fixed;
+  top: 0;
+  right: 100px;
+  width: 147.5px;
+  height: 172.5px;
+  background-image: url(~/assets/img/taogirl_L.png);
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.fix-taogirl-L {
+  position: fixed;
+  top: 0;
+  left: 100px;
+  width: 147.5px;
+  height: 172.5px;
+  background-image: url(~/assets/img/taogirl_R.png);
+  background-repeat: no-repeat;
+  background-size: cover;
 }
 </style>
