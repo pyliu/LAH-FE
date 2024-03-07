1193e214c4b6145d8fdb3bd0fdf727faf3b3983f
pyliu <3634168+pyliu@users.noreply.github.com>
2024-03-07 09:29:55 +0800

    wording

--- components/lah-monitor-board-sms.vue
+++ components/lah-monitor-board-sms.vue
@@ -49,21 +49,21 @@ b-card(:border-variant="border")
     section(v-else)
       .d-flex.justify-content-between.align-items-center
         lah-fa-icon.font-weight-bold(icon="circle", :variant="firstNotifyLight") 地籍異動即時通
-        b-link(@click="popupSingleSMS(firstNotifyLog)") {{ firstNotifyLog.SMS_CELL || '尚無發送簡訊' }}
+        b-link(@click="popupSingleSMS(firstNotifyLog)", title="顯示簡訊內容視窗") {{ firstNotifyLog.SMS_CELL || '本日尚無發送簡訊' }}
         lah-badge-human-datetime(:seconds="convertSeconds(firstNotifyLog)")
-      .truncate.small.text-muted: b-link(@click="popupSingleSMS(firstNotifyLog)") {{ isSuccess(firstNotifyLog) ? firstNotifyLog.SMS_CONTENT : firstNotifyLog.SMS_RESULT }}
+      .truncate.small.text-muted: b-link(@click="popupSingleSMS(firstNotifyLog)", title="顯示簡訊內容視窗") {{ isSuccess(firstNotifyLog) ? firstNotifyLog.SMS_CONTENT : firstNotifyLog.SMS_RESULT }}
       hr
       .d-flex.justify-content-between.align-items-center
         lah-fa-icon.font-weight-bold(icon="circle", :variant="firstCaseLight") 案件辦理情形&emsp;
-        b-link(@click="popupSingleSMS(firstCaseLog)") {{ firstCaseLog.SMS_CELL || '尚無發送簡訊' }}
+        b-link(@click="popupSingleSMS(firstCaseLog)", title="顯示簡訊內容視窗") {{ firstCaseLog.SMS_CELL || '本日尚無發送簡訊' }}
         lah-badge-human-datetime(:seconds="convertSeconds(firstCaseLog)")
-      .truncate.small.text-muted: b-link(@click="popupSingleSMS(firstCaseLog)") {{ isSuccess(firstCaseLog) ? firstCaseLog.SMS_CONTENT : firstCaseLog.SMS_RESULT }}
+      .truncate.small.text-muted: b-link(@click="popupSingleSMS(firstCaseLog)", title="顯示簡訊內容視窗") {{ isSuccess(firstCaseLog) ? firstCaseLog.SMS_CONTENT : firstCaseLog.SMS_RESULT }}
       hr
       .d-flex.justify-content-between.align-items-center
         lah-fa-icon.font-weight-bold(icon="circle", :variant="firstOtherLight") 其他簡訊&emsp;&emsp;&emsp;
-        b-link(@click="popupSingleSMS(firstOtherLog)") {{ firstOtherLog.SMS_CELL || '尚無發送簡訊' }}
+        b-link(@click="popupSingleSMS(firstOtherLog)", title="顯示簡訊內容視窗") {{ firstOtherLog.SMS_CELL || '本日尚無發送簡訊' }}
         lah-badge-human-datetime(:seconds="convertSeconds(firstOtherLog)")
-      .truncate.small.text-muted: b-link(@click="popupSingleSMS(firstOtherLog)") {{ isSuccess(firstOtherLog) ? firstOtherLog.SMS_CONTENT : firstOtherLog.SMS_RESULT }}
+      .truncate.small.text-muted: b-link(@click="popupSingleSMS(firstOtherLog)", title="顯示簡訊內容視窗") {{ isSuccess(firstOtherLog) ? firstOtherLog.SMS_CONTENT : firstOtherLog.SMS_RESULT }}
 
   template(#footer, v-if="footer"): client-only: lah-monitor-board-footer(
     ref="footer"
