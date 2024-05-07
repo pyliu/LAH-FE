9c9ec27bf2d6c4ceaa40d5736c8043608be1350d
pyliu <3634168+pyliu@users.noreply.github.com>
2024-05-07 11:20:43 +0800

    add redState

--- components/lah-reg-tracking-cards.vue
+++ components/lah-reg-tracking-cards.vue
@@ -1,6 +1,10 @@
 <template lang="pug">
 transition-group.d-flex.justify-content-between.flex-wrap.lah(name="list")
-  b-card(v-for="(row, idx) in rows", :key="`tracking_card_${idx}`")
+  b-card(
+    v-for="(row, idx) in rows",
+    :key="`tracking_card_${idx}`",
+    :border-variant="border(row)"
+  )
     .center.h1.text-primary.font-weight-bold(@click="popup(row)") {{ row['收件字'] }} {{ row.RM03 }}
     .center.h2.my-2.font-weight-bold {{ row['登記原因'] }}
     .center
@@ -25,7 +29,8 @@ export default {
   },
   data: () => ({
     greenState: ['結案', '異動完成', '歸檔'],
-    yellowState: ['登錄', '校對']
+    yellowState: ['登錄', '校對'],
+    redState: ['撤回', '駁回', '請示', '展期']
   }),
   computed: {},
   watch: {},
@@ -39,7 +44,14 @@ export default {
       if (this.yellowState.includes(row['辦理情形'])) {
         return 'warning'
       }
+      if (this.redState.includes(row['辦理情形'])) {
+        return 'danger'
+      }
       return 'info'
+    },
+    border (row) {
+      const light = this.light(row)
+      return light === 'info' ? '' : light
     },
     hideName (name) {
       return name[0] + 'Ｏ' + name.slice(2)
