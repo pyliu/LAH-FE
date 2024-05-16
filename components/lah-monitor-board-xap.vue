12a339b6896356059f81762071d9a588712c6d16
pyliu <3634168+pyliu@users.noreply.github.com>
2024-05-16 16:34:14 +0800

    add help message

--- components/lah-monitor-board-xap.vue
+++ components/lah-monitor-board-xap.vue
@@ -26,8 +26,9 @@ b-card(no-body, :border-variant="borderVariant")
     lah-help-modal(:modal-id="modalId", :modal-title="`${header} 監控說明`")
       ul
         li 顯示跨域AP連線數統計狀態 (⭐ AP需安裝#[a(:href="netstatsSh" target="_blank") 回報腳本] ⭐)
-        li #[lah-fa-icon(icon="database", variant="danger")] 顯示資料庫連線總數(超過3000會造成地政系統停擺)
-        li #[lah-fa-icon(icon="wave-square", variant="success")] 顯示AP連線總數(數字越高有可能造成地政系統回應緩慢)
+        li AP連線設定檔路徑 /opt/jboss/server/default/deploy/moiland-ds.xml
+        li #[lah-fa-icon(icon="database", variant="danger")] 顯示資料庫連線總數(超過3000會造成地政系統停擺, 桃園所AP端設定上限2500)
+        li #[lah-fa-icon(icon="wave-square", variant="success")] 顯示AP連線總數(桃園所AP端設定最高990)
         li #[lah-fa-icon(icon="clock", regular)] 顯示資料更新時間
         li 15秒更新資料一次
 
