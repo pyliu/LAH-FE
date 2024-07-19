092f61bae7b8381df1d845629b6f710e53953d53
pyliu <3634168+pyliu@users.noreply.github.com>
2024-07-19 13:47:59 +0800

    shorten debounce time duration

--- pages/inf/dashboard/index.vue
+++ pages/inf/dashboard/index.vue
@@ -274,7 +274,7 @@ export default {
       }
       // order by state
       this.attentionList = this.$utils.orderBy(tmp, 'state')
-    }, 5000)
+    }, 1000)
     // using animation to catch attention
     this.attentionTimer = setInterval(() => {
       this.dangerList.forEach((card) => {
