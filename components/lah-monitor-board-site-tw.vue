5d87f57a38d590b75322dd684eb3d5885307cfd5
pyliu <3634168+pyliu@users.noreply.github.com>
2024-08-06 09:50:32 +0800

    add .justify-content-evenly to the flex item

--- components/lah-monitor-board-site-tw.vue
+++ components/lah-monitor-board-site-tw.vue
@@ -69,7 +69,7 @@ b-card(:border-variant="borderVariant", :class="[attentionCss]")
       .mt-3.text-center(v-else-if="downCount === 0 && displayError")
         lah-fa-icon.h4(icon="circle-check", variant="success") 無偵測到異常
 
-    .d-flex.flex-wrap(
+    .d-flex.flex-wrap.justify-content-evenly(
       :class="displayError ? [] : ['justify-content-between']"
     )
       lah-badge-site-status.m-1(
