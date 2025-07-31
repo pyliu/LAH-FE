f103f4168fcee0b749ba145ca9653416e0a92586
pyliu <3634168+pyliu@users.noreply.github.com>
2025-07-31 15:10:03 +0800

    tweak download UI

--- components/lah-xlsx-sur-tracking-download.vue
+++ components/lah-xlsx-sur-tracking-download.vue
@@ -11,16 +11,21 @@ b-card(
       title="點我產製EXCEL檔案",
       size="lg",
       @click="process"
-    ) 開始產製
-    lah-button.my-1(
-      v-else
-      :href="downloadUrl",
-      icon="download",
-      title="下載目前產製的EXCEL檔案",
-      size="lg",
-      variant="success",
-      link
-    ) 手動下載
+    ) {{ isBusy ? '產製中...勿關閉視窗' : '開始產製' }}
+    .my-1(v-else)
+      h6 ✅ 產製完成，請查看下載視窗(Ctrl + J)另存新檔後開啟。
+      .d-flex.justify-content-end.align-items-center.small.text-muted
+        span 若無自動下載請按
+        b-link.mx-1(:href="downloadUrl", target="_blank") 這裡
+        span 手動下載
+      //- lah-button.my-1(
+      //-   :href="downloadUrl",
+      //-   icon="download",
+      //-   title="下載目前產製的EXCEL檔案",
+      //-   size="lg",
+      //-   variant="success",
+      //-   link
+      //- ) 手動下載
 
   lah-fa-icon(icon="list-alt", variant="secondary") 處理紀錄
   b-list-group(flush): b-list-group-item(v-for="(msg, idx) in messages" :key="`${idx}_msg`")
