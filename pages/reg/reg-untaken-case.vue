1b9b1a3ba7cce05dfe386386d965e9d6eafb6d3c
pyliu <3634168+pyliu@users.noreply.github.com>
2025-09-11 15:08:30 +0800

    tweak

--- pages/reg/reg-untaken-case.vue
+++ pages/reg/reg-untaken-case.vue
@@ -276,7 +276,7 @@ div
     title="批次修改領件狀態",
     no-close-on-backdrop,
     :hide-header-close="batchProcessing",
-    size="xl"
+    size="md"
   )
     b-form-fieldset(:disabled="batchProcessing")
       .d-flex
@@ -1117,7 +1117,7 @@ export default {
   font-size: 0.95rem;
 }
 // trick to limit batch update modal width
-::v-deep .modal-dialog.modal-xl {
-  max-width: 525px;
+::v-deep .modal-dialog.modal-lg {
+  width: 685px;
 }
 </style>
