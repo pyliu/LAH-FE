55d28fac6e190c611bc9389989ade8955414a3ee
pyliu <3634168+pyliu@users.noreply.github.com>
2026-02-02 10:20:20 +0800

    use flex-start to order items in top-down left-right

--- components/lah-flex-item-group.vue
+++ components/lah-flex-item-group.vue
@@ -21,13 +21,22 @@ export default {
 .lah {
   display: flex;
   flex-wrap: wrap;
-  // justify-content: space-evenly;
-  justify-content: space-between;
+  // [修改] 改用 flex-start 避免最後一行項目分散兩端
+  justify-content: flex-start;
   align-items: stretch; /* Stretch child elements to fill the height */
+
   > * {
-    // align-items: stretch; /* Stretch child elements to fill the height */
     margin-bottom: 1.25rem;
+    // [新增] 計算間距：(容器寬度 - 3個卡片寬度) / 2個間隙
+    // 這樣可以模擬 space-between 的效果，但保持靠左對齊
+    margin-right: calc((100% - (31vw * 3)) / 2);
+
+    // [新增] 每一行的第 3 個元素（最右邊那個）不需要 margin-right
+    &:nth-child(3n) {
+      margin-right: 0;
+    }
   }
+
   > .card {
     width: 31vw;
     height: calc((100vh - 150px) / 3);
