8b11602df40044c8711e1e93466fef18148f37b4
pyliu <3634168+pyliu@users.noreply.github.com>
2026-02-04 16:43:39 +0800

    remove useless logo

--- components/lah-logo.vue
+++ components/lah-logo.vue
@@ -3,13 +3,13 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        mainProps: { blank: false, width: 220, height: 220, class: 'mb-2 lah-shadow' }
-      }
+export default {
+  data () {
+    return {
+      mainProps: { blank: false, width: 220, height: 220, class: 'mb-2 lah-shadow' }
     }
   }
+}
 </script>
 
 <style lang="scss">
